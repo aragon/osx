b8b34b14e75d46764809c832aa0e0ff78b69b0c9
sepehr <sepehr2mail@gmail.com>
2021-10-22 13:01:44 +0330

    add react-i18next as dev dependencies

--- packages/web-app/react-i18next.d.ts
+++ packages/web-app/react-i18next.d.ts
@@ -1,13 +1,7 @@
-import { resources, defaultNS } from './i18n.config';
-// react-i18next versions lower than 11.11.0
-declare module 'react-i18next' {
-  type DefaultResources = typeof resources['en'];
-  interface Resources extends DefaultResources {}
-}
-// react-i18next versions higher than 11.11.0
+import { resources } from './i18n.config'
+
 declare module 'react-i18next' {
   interface CustomTypeOptions {
-    defaultNS: typeof defaultNS;
-    resources: typeof resources['en'];
-  };
-};
+    resources: typeof resources['en']
+  }
+}
