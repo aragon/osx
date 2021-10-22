e5c0af558c02725212668b94042482f6b0b599bc
sepehr <sepehr2mail@gmail.com>
2021-10-22 13:35:10 +0330

    add use Wallet package

--- packages/web-app/src/index.tsx
+++ packages/web-app/src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'tailwindcss/tailwind.css'
+import { WalletProvider } from './providers/AugmentedWallet'
 import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <WalletProvider>
+      <App />
+    </WalletProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
