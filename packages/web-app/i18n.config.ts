7577c85eb0b09d0c4455ec5709bfd862610a0399
Fabrice Francois <fabricevladimir@gmail.com>
2021-10-26 06:44:01 -0400

    added base folder structure

--- packages/web-app/i18n.config.ts
+++ packages/web-app/i18n.config.ts
@@ -1,6 +1,7 @@
 import i18n from 'i18next'
-import en from './src/languages/en.json'
 import { initReactI18next } from 'react-i18next'
+
+import en from './src/locales/en/translation.json'
 
 export const resources = {
   en: {
