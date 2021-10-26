3ed97c390549e900f22d31eb4edba628ba2913dc
Ramon Canales <emaildoramon@gmail.com>
2021-10-26 16:17:03 +0100

    adding es as supported language

--- packages/web-app/i18n.config.ts
+++ packages/web-app/i18n.config.ts
@@ -14,6 +14,6 @@ i18n.use(initReactI18next).init({
   interpolation: {
     escapeValue: false // react already safes from xss
   },
-  supportedLngs: ['en', 'pt'],
+  supportedLngs: ['en', 'pt', 'es'],
   fallbackLng: 'en'
 })
