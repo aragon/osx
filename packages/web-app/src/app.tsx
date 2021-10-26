6c21f96c20567de3068ba13679cac0ef88ed7181
Fabrice Francois <fabricevladimir@gmail.com>
2021-10-26 11:51:18 -0400

    added basic routing

--- packages/web-app/src/app.tsx
+++ packages/web-app/src/app.tsx
@@ -1,20 +1,30 @@
 import React from 'react'
+import { Route, HashRouter as Router, Switch } from 'react-router-dom'
 
 import '../i18n.config'
 
-import Home from './pages/home'
 import Footer from './containers/footer'
 import Navbar from './containers/navbar'
+import { PageRoute, routes } from './routes'
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
       <div className="h-screen">
-        <Home />
+        <Switch>
+          {routes.map((route: PageRoute) => (
+            <Route
+              key={route.name}
+              path={route.path}
+              exact
+              component={route.component}
+            />
+          ))}
+        </Switch>
       </div>
       <Footer />
-    </>
+    </Router>
   )
 }
 
---
+++ packages/web-app/src/pages/notFound.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NotFound: React.FC = () => {
+  return <h1>404 Page not found</h1>
+}
+
+export default React.memo(NotFound)
