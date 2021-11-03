a2668a051a56a1a297e41204d67bae03a3ef1520
Fabrice Francois <fabricevladimir@gmail.com>
2021-11-03 09:04:43 -0400

    imported button component from ui-components package

--- packages/web-app/tailwind.config.js
+++ packages/web-app/tailwind.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  mode: 'jit',
+  // mode: 'jit',
   purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {},
   },
   variants: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
-}
+  plugins: [],
+};
