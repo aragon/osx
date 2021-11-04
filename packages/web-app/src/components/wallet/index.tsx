6771b63ade0d97a7d88b768e7754b62ca0577919
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-11-04 17:34:20 +0100

    changed linking between packages

--- packages/web-app/src/components/wallet/index.tsx
+++ packages/web-app/src/components/wallet/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import {Button} from 'ui-components';
+import {Button} from '@aragon/ui-components';
 
 import {useWallet} from 'context/augmentedWallet';
 
