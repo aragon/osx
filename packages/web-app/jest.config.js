1901c73f36388bdcec65cc9b2d3fb18f81d2ddd2
Fabrice Francois <fabricevladimir@gmail.com>
2021-11-08 09:57:02 -0500

    added test setup

--- packages/web-app/jest.config.js
+++ packages/web-app/jest.config.js
@@ -4,5 +4,10 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']
-}
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
+  moduleDirectories: [
+    'node_modules', // add the directory with the test-utils.js file, for example:
+    'utils', // a utility folder
+    __dirname, // the root directory
+  ],
+};
