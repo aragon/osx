402a772ec21073310192bf3b15269b7dc7f22e4c
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-11-09 15:23:31 +0100

    Modified Story

--- packages/ui-components/stories/Button.stories.tsx
+++ packages/ui-components/stories/Button.stories.tsx
@@ -5,19 +5,12 @@ import {Button, ButtonProps} from '../src';
 export default {
   title: 'Components/Button',
   component: Button,
-  argTypes: {
-    backgroundColor: {control: 'color'},
-  },
 } as Meta;
 
 const Template: Story<ButtonProps> = args => <Button {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
+export const AllProps = Template.bind({});
+AllProps.args = {
   label: 'Button Text',
   disabled: false,
-  onClick: () => console.log('hi'),
-  // mode: 'primary',
-  // size: 'default',
-  // functionality: 'default',
 };
