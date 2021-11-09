f3636f1c4cae0637ae8935761e8369817bd1fa33
Fabrice Francois <fabricevladimir@gmail.com>
2021-11-09 09:38:21 -0500

    updated moduleDirectories comment

--- packages/web-app/jest.config.js
+++ packages/web-app/jest.config.js
@@ -5,9 +5,7 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.ts(x)'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
-  moduleDirectories: [
-    'node_modules', // add the directory with the test-utils.js file, for example:
-    'utils', // a utility folder
-    __dirname, // the root directory
-  ],
+  // moduleDirectories overrides default jest package lookup behavior
+  // using this to include utils folder so jest is aware of where the test-utils file resides
+  moduleDirectories: ['node_modules', 'utils', __dirname],
 };
