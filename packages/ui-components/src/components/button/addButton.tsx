197ee983b993132b44a63adcffe245ac29cc072a
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-11-10 13:47:28 +0100

    added "add button"

---
+++ packages/ui-components/src/components/button/addButton.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+import {IconButton} from './button';
+
+export const AddButton: React.FC<ButtonProps> = ({
+  mode = 'primary',
+  size = 'default',
+  disabled = false,
+  label,
+  onClick,
+}) => {
+  // TODO replace with correct icon src [VR 10-11-2021]
+  const addIcon = 'https://place-hold.it/150x150';
+  return (
+    <IconButton
+      iconSrc={addIcon}
+      label={label}
+      side="left"
+      mode={mode}
+      size={size}
+      disabled={disabled}
+    />
+  );
+};
