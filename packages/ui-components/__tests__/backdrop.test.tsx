07e56f899993697d19cd12134e96becb4880a34c
Fabrice Francois <fabricevladimir@gmail.com>
2021-11-12 06:09:01 -0500

    added minimal jest setup and test for actionItem

--- packages/ui-components/__tests__/backdrop.test.tsx
+++ packages/ui-components/__tests__/backdrop.test.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
 
-// TODO: add at setup
-import '@testing-library/jest-dom/extend-expect';
-
 import {Default as Backdrop} from '../stories/Backdrop.stories';
 
 describe('Backdrop', () => {
--- packages/ui-components/__tests__/popover.test.tsx
+++ packages/ui-components/__tests__/popover.test.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
 
-// TODO: add at setup
-import '@testing-library/jest-dom/extend-expect';
-
 import {Default as Popover} from '../stories/Popover.stories';
 
 describe('Popover', () => {
