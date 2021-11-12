a9e4a86feaed9a38930e7ab73cb38f48a9a11b43
Fabrice Francois <fabricevladimir@gmail.com>
2021-11-12 05:35:10 -0500

    added icon toggle state

--- packages/ui-components/src/components/actionItem/actionItem.tsx
+++ packages/ui-components/src/components/actionItem/actionItem.tsx
@@ -1,39 +1,74 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import {IconButton} from '../../../src';
-
 export type ActionItemProps = {
+  /**
+   * Whether the button is enabled
+   */
+  disabled?: boolean;
+  /**
+   * Icon to prepend to the button text
+   */
+  icon: string; //TODO: find library Icon type and set
+  /**
+   * Button text
+   */
   label: string;
-  icon: string;
+  onClick?: () => void;
+  /**
+   * Shows the icon only when the ActionItem is active
+   */
+  toggleIcon?: boolean;
 };
 
 // TODO: Test
-// TODO: configure boxshadow for focus
+/**
+ * Action UI component used for list groups and navigation links
+ */
 export const ActionItem: React.FC<ActionItemProps> = ({
+  disabled = false,
   icon,
   label,
-  ...props
+  onClick,
+  toggleIcon = false,
 }) => {
   return (
-    <StyledIconB
-      mode="ghost"
-      //  'text-primary-500 bg-ui-0 hover:text-primary-800 active:bg-primary-50';
-      iconSrc={icon}
-      label={label}
-      // TODO:  Check iconbutton api
-      size="default"
-      side="left"
-      {...props}
-    />
+    <StyledActionItem
+      onClick={onClick}
+      disabled={disabled}
+      data-testid="actionItem"
+    >
+      <Content>
+        <IconContainer toggleIcon={toggleIcon}>{icon}</IconContainer>
+        <Label>{label}</Label>
+      </Content>
+    </StyledActionItem>
   );
 };
 
-const StyledIconB = styled(IconButton).attrs(({className}) => {
-  console.log(className);
+const StyledActionItem = styled.button.attrs({
+  className: `rounded-xl py-1.5 px-2 text-base text-ui-600 bg-ui-0
+    hover:text-primary-500 active:bg-primary-50 disabled:text-ui-300 disabled:bg-ui-0`,
 })``;
 
-const StyledIconButton = styled(IconButton).attrs({
-  className:
-    'font-semibold text-base text-ui-600 hover:text-primary-500 disabled:text-ui-300',
+const Label = styled.p``;
+
+const Content = styled.div.attrs({
+  className: 'flex items-center space-x-1.5',
 })``;
+
+type IconContainerProps = {toggleIcon: boolean};
+const IconContainer = styled.div.attrs({
+  className: 'w-2 h-2 border',
+})<IconContainerProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  // If toggleIcon, hide icon on all states except for active
+  ${({toggleIcon}) =>
+    toggleIcon &&
+    `display: none; 
+     ${StyledActionItem}:active & {display: flex};
+    `}
+`;
