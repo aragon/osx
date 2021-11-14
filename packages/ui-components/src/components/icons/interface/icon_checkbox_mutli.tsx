5008d36f98f41687c62647c5e978fb5e8ed7d396
Sepehr sanaee <sepehr_sanaee78@yahoo.com>
2021-11-14 14:33:31 +0330

    re-export icons

--- packages/ui-components/src/components/icons/interface/icon_checkbox_mutli.tsx
+++ packages/ui-components/src/components/icons/interface/icon_checkbox_mutli.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import {IconType} from '..';
 
 export const IconCheckboxMulti: IconType = ({
-  height = 24,
-  width = 24,
+  height = 16,
+  width = 16,
   ...props
 }) => {
   return (
@@ -11,7 +11,7 @@ export const IconCheckboxMulti: IconType = ({
       width={width}
       height={height}
       fill="none"
-      viewBox="0 0 24 24"
+      viewBox="0 0 16 16"
       {...props}
     >
       <path
