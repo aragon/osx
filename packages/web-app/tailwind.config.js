7727eb3a0eea08871229fd6ab768318d8b4dda64
Fabrice Francois <fabricevladimir@gmail.com>
2021-12-14 11:41:53 -0500

    temporarily removed css purging (#94)

--- packages/web-app/tailwind.config.js
+++ packages/web-app/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./src/**/*.html', './src/**/*.tsx'],
+  purge: false, // purge: ['./src/**/*.html', './src/**/*.tsx'], TODO: investigate
   darkMode: false,
   theme: {
     extend: {
