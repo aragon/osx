33b380a32fd3b111a25aa68c96bd9937ae2bd510
Fabrice Francois <fabricevladimir@gmail.com>
2022-02-09 06:03:14 -0500

    Fix/Staging bug - DAO-689 (#215)
    * removed double export
    
    * explicitly export types

--- packages/ui-components/src/components/input/index.ts
+++ packages/ui-components/src/components/input/index.ts
@@ -1,9 +1,25 @@
-export {TextInputProps, TextInput} from './textInput';
-export {SearchInput, SearchInputProps} from './searchInput';
-export {NumberInput, NumberInputProps} from './numberInput';
-export {DropdownInput, DropDownInputProps} from './dropdownInput';
-export {TimeInput, TimeInputProps} from './timeInput';
-export {DateInput, DateInputProps} from './dateInput';
+/**
+ * Types have to be explicitly re - exported(export type { abc } from 'component')
+ * or exported with export * from 'component'; in TS 3.7 or older when the
+ * --isolatedModules flag is provided
+ */
+export {TextInput} from './textInput';
+export type {TextInputProps} from './textInput';
+
+export {SearchInput} from './searchInput';
+export type {SearchInputProps} from './searchInput';
+
+export {NumberInput} from './numberInput';
+export type {NumberInputProps} from './numberInput';
+
+export {DropdownInput} from './dropdownInput';
+export type {DropDownInputProps} from './dropdownInput';
+
+export {TimeInput} from './timeInput';
+export type {TimeInputProps} from './timeInput';
+
+export {DateInput} from './dateInput';
+export type {DateInputProps} from './dateInput';
+
 export * from './inputImageSingle';
 export * from './valueInput';
-export * from './dateInput';
