a7a11756475b1fe42f37c4b43d261a4619e0973b
Jør∂¡ <itsbrickpop@gmail.com>
2022-03-29 13:31:55 +0200

    Leave typechain for contract testing

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 import {HardhatUserConfig, task} from 'hardhat/config';
 import '@nomiclabs/hardhat-etherscan';
 import '@nomiclabs/hardhat-waffle';
+import '@typechain/hardhat';
 import 'hardhat-deploy';
 import 'hardhat-gas-reporter';
 import 'solidity-coverage';
