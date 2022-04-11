8ba38ab73ac5b537aed7b7f9b057c167dc55acac
Michael Andree Heuer <heuer@pc.rwth-aachen.de>
2022-04-11 19:36:44 +0200

    Added mock component and test

---
+++ packages/contracts/test/core/component-mock.ts
@@ -0,0 +1,47 @@
+import chai, { expect } from 'chai';
+import { ethers } from 'hardhat';
+import chaiUtils from '../test-utils';
+import { ERRORS } from '../test-utils/custom-error-helper';
+
+chai.use(chaiUtils);
+
+import { ComponentMock, DAOMock } from '../../typechain';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+
+describe('ComponentMock', function () {
+  let signers: SignerWithAddress[];
+  let componentMock: ComponentMock;
+  let daoMock: DAOMock;
+  let ownerAddress: string;
+
+  before(async () => {
+    signers = await ethers.getSigners();
+    ownerAddress = await signers[0].getAddress();
+
+    const DAOMock = await ethers.getContractFactory('DAOMock');
+    daoMock = await DAOMock.deploy(ownerAddress);
+  });
+
+  beforeEach(async () => {
+    const ComponentMock = await ethers.getContractFactory('ComponentMock');
+    componentMock = await ComponentMock.deploy();
+
+    await componentMock.initialize(daoMock.address);
+  });
+
+  describe('Initialization', async () => {
+    it('reverts if trying to re-initialize', async () => {
+      await expect(
+        componentMock.initialize(daoMock.address)
+      ).to.be.revertedWith(ERRORS.ALREADY_INITIALIZED);
+    });
+  });
+
+  describe('Context: ', async () => {
+    it('returns the right message sender', async () => {
+      expect(
+        await componentMock.msgSender()
+      ).to.be.equal(ownerAddress);
+    });
+  });
+});
