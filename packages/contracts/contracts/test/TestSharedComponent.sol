4355b420ea870a91e189f0c2e540d785be577fa7
Michael Heuer <michael.heuer@rwth-aachen.de>
2022-05-18 08:02:44 +0200

    Added ACLOracle integration test to ensure building components shared by multiple DAOs is possible

---
+++ packages/contracts/contracts/test/TestSharedComponent.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.10;
+
+import "../core/component/Component.sol";
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+/// @notice A test component that manages permission to internal objects by associating their IDs with specific DAOs. Only the DAO for which the object was created has the permission to perform ID-gated actions on them.
+/// @dev This is realized by asking an `IACLOracle` that must be authorized in the DAO's ACL.
+contract TestSharedComponent is Component {
+    bytes32 public constant ID_GATED_ACTION_ROLE = keccak256("ID_GATED_ACTION_ROLE");
+
+    uint256 internal _counter;
+
+    modifier sharedAuth(uint256 _id, bytes32 _role) {
+        if (!ownedIds[_id].hasPermission(address(this), _msgSender(), _role, _msgData()))
+            revert ACLData.ACLAuth({here: address(this), where: address(this), who: _msgSender(), role: _role});
+
+        _;
+    }
+    mapping(uint256 => IDAO) public ownedIds;
+
+    function initialize(IDAO _dao) external initializer {
+        __Component_init(_dao);
+    }
+
+    /// @notice Creates a new object with an ID being associated with a specific DAO
+    /// @param _dao The DAO that manages permissions for the object
+    /// @return id The ID that is associated with the object and the DAO
+    function newObject(IDAO _dao) external returns (uint256 id) {
+        id = _counter;
+        ownedIds[id] = _dao;
+        _counter++;
+    }
+
+    /// @notice Executes something if the `id` parameter is authorized by the DAO associated through `ownedIds`. This is done by asking an `IACLOracle` that must be authorized in the DAO's ACL via `grantWithoracle` and the `ID_GATED_ACTION_ROLE`.
+    /// @param _id The ID that is  associated with a specific DAO
+    function idGatedAction(uint256 _id) external sharedAuth(_id, ID_GATED_ACTION_ROLE) {
+        // do something
+    }
+}
+
+/// @notice The oracle associated
+contract TestIdGatingOracle is IACLOracle {
+    uint256 public allowedId;
+
+    constructor(uint256 _id) {
+        allowedId = _id;
+    }
+
+    /// @notice Checks the calldata and expects the `id` to be the first argument of type `uint256`
+
+    function willPerform(
+        address _where,
+        address _who,
+        bytes32 _role,
+        bytes calldata _data
+    ) external view returns (bool) {
+        (_where, _who, _role);
+
+        // Security issue? Can the method be wrapped?
+
+        // Skip the signature (the first 4 bytes) and decode the first parameter as a `uint256`
+        uint256 id = abi.decode(_data[4:], (uint256));
+
+        // let perform if the id match
+        return id == allowedId;
+    }
+}
---
+++ packages/contracts/test/core/acl/shared-component.ts
@@ -0,0 +1,196 @@
+import chai, {expect} from 'chai';
+import {ethers} from 'hardhat';
+import chaiUtils from '../../test-utils';
+import {customError} from '../../test-utils/custom-error-helper';
+
+chai.use(chaiUtils);
+
+import {TestSharedComponent, TestIdGatingOracle, DAO} from '../../../typechain';
+import {SignerWithAddress} from '@nomiclabs/hardhat-ethers/signers';
+
+const ID_GATED_ACTION_ROLE = ethers.utils.id('ID_GATED_ACTION_ROLE');
+
+describe('SharedComponent', function () {
+  let signers: SignerWithAddress[];
+  let testComponent: TestSharedComponent;
+  let owningDao: DAO;
+  let dao1: DAO;
+  let dao2: DAO;
+  let ownerAddress: string;
+  let expectedACLAuthError: string;
+
+  beforeEach(async () => {
+    signers = await ethers.getSigners();
+    ownerAddress = await signers[0].getAddress();
+
+    // create a DAO
+    const DAO = await ethers.getContractFactory('DAO');
+    owningDao = await DAO.deploy();
+    dao1 = await DAO.deploy();
+    dao2 = await DAO.deploy();
+    await owningDao.initialize(
+      '0x',
+      ownerAddress,
+      ethers.constants.AddressZero
+    );
+    await dao1.initialize('0x', ownerAddress, ethers.constants.AddressZero);
+    await dao2.initialize('0x', ownerAddress, ethers.constants.AddressZero);
+
+    // create a component
+    const TestSharedComponent = await ethers.getContractFactory(
+      'TestSharedComponent'
+    );
+    testComponent = await TestSharedComponent.deploy();
+    await testComponent.initialize(owningDao.address);
+
+    expectedACLAuthError = customError(
+      'ACLAuth',
+      testComponent.address,
+      testComponent.address,
+      ownerAddress,
+      ID_GATED_ACTION_ROLE
+    );
+  });
+
+  it('increments ids', async () => {
+    expect(await testComponent.callStatic.newObject(dao1.address)).to.be.equal(
+      0
+    );
+
+    const tx = await testComponent.newObject(dao1.address);
+    await tx.wait();
+    await ethers.provider.send('evm_mine', []);
+
+    expect(await testComponent.callStatic.newObject(dao1.address)).to.be.equal(
+      1
+    );
+  });
+
+  describe('idGatedAction:', async () => {
+    let oracle: TestIdGatingOracle;
+
+    beforeEach(async () => {});
+    it('executes if the id is allowed', async () => {
+      // deploy oracle and set allowed id
+      const allowedId = 0;
+
+      const Oracle = await ethers.getContractFactory('TestIdGatingOracle');
+      oracle = await Oracle.deploy(allowedId);
+
+      // grant signer[0] permission to do id gated actions with the deployed oracle
+      dao1.grantWithOracle(
+        testComponent.address,
+        ownerAddress,
+        ID_GATED_ACTION_ROLE,
+        oracle.address
+      );
+
+      // Create component with id 0
+      const tx = await testComponent.newObject(dao1.address);
+      await tx.wait();
+      await ethers.provider.send('evm_mine', []);
+
+      expect(testComponent.callStatic.idGatedAction(0));
+    });
+
+    it('reverts if the id is not allowed', async () => {
+      // deploy oracle and set allowed id
+      const allowedId = 1;
+
+      const Oracle = await ethers.getContractFactory('TestIdGatingOracle');
+      oracle = await Oracle.deploy(allowedId);
+
+      // grant signer[0] permission to do id gated actions with the deployed oracle
+      dao1.grantWithOracle(
+        testComponent.address,
+        ownerAddress,
+        ID_GATED_ACTION_ROLE,
+        oracle.address
+      );
+
+      // create id-gated object associated with dao1
+      const tx = await testComponent.newObject(dao1.address);
+      await tx.wait();
+      await ethers.provider.send('evm_mine', []);
+
+      await expect(
+        testComponent.callStatic.idGatedAction(0)
+      ).to.be.revertedWith(expectedACLAuthError);
+    });
+
+    it('reverts if the role name is wrong', async () => {
+      // deploy oracle and set allowed id
+      const allowedId = 1;
+
+      const Oracle = await ethers.getContractFactory('TestIdGatingOracle');
+      oracle = await Oracle.deploy(allowedId);
+
+      // grant signer[0] permission to do id gated actions with the deployed oracle
+      dao1.grantWithOracle(
+        testComponent.address,
+        ownerAddress,
+        ethers.utils.id('WRONG_ROLE'),
+        oracle.address
+      );
+
+      // create id-gated object associated with dao1
+      const tx = await testComponent.newObject(dao1.address);
+      await tx.wait();
+      await ethers.provider.send('evm_mine', []);
+
+      await expect(
+        testComponent.callStatic.idGatedAction(0)
+      ).to.be.revertedWith(expectedACLAuthError);
+    });
+
+    it('reverts if the permission is set in the wrong DAO', async () => {
+      // deploy oracle and set allowed id
+      const allowedId = 0;
+
+      const Oracle = await ethers.getContractFactory('TestIdGatingOracle');
+      oracle = await Oracle.deploy(allowedId);
+
+      // grant signer[0] permission to do id gated actions with the deployed oracle
+      dao2.grantWithOracle(
+        testComponent.address,
+        ownerAddress,
+        ID_GATED_ACTION_ROLE,
+        oracle.address
+      );
+
+      // create id-gated object associated with dao1
+      const tx = await testComponent.newObject(dao1.address);
+      await tx.wait();
+      await ethers.provider.send('evm_mine', []);
+
+      await expect(
+        testComponent.callStatic.idGatedAction(0)
+      ).to.be.revertedWith(expectedACLAuthError);
+    });
+
+    it('reverts if the object belongs to the wrong DAO', async () => {
+      // deploy oracle and set allowed id
+      const allowedId = 0;
+
+      const Oracle = await ethers.getContractFactory('TestIdGatingOracle');
+      oracle = await Oracle.deploy(allowedId);
+
+      // grant signer[0] permission to do id gated actions with the deployed oracle
+      dao1.grantWithOracle(
+        testComponent.address,
+        ownerAddress,
+        ID_GATED_ACTION_ROLE,
+        oracle.address
+      );
+
+      // create id-gated object associated with dao1
+      const tx = await testComponent.newObject(dao2.address);
+      await tx.wait();
+      await ethers.provider.send('evm_mine', []);
+
+      await expect(
+        testComponent.callStatic.idGatedAction(0)
+      ).to.be.revertedWith(expectedACLAuthError);
+    });
+  });
+});
