e5b2e6922e1e2bab37c57a1145bc6d12379dbbda
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2022-06-16 02:11:54 +0200

    update change log

--- packages/contracts/contracts/factory/RepoFactory.sol
+++ packages/contracts/contracts/factory/RepoFactory.sol
@@ -7,6 +7,9 @@ pragma solidity 0.8.10;
 import "../registry/APMRegistry.sol";
 import "../APM/Repo.sol";
 
+/// @title RepoFactory to create a Repo
+/// @author Sarkawt Noori - Aragon Association - 2022
+/// @notice This contract is used to create a Repo and register it on APMRegistry contract.
 contract RepoFactory {
     APMRegistry apmRegistry;
 
@@ -16,21 +19,17 @@ contract RepoFactory {
         apmRegistry = _apmRegistry;
     }
 
-    /**
-     * @notice Create new repo in registry with `_name`
-     * @param _name Repo name, must be ununsed
-     */
+    /// @notice Create new repo in registry with `_name`
+    /// @param _name Repo name, must be ununsed
     function newRepo(string calldata _name) external returns (Repo) {
         return _newRepo(_name, msg.sender);
     }
 
-    /**
-     * @notice Create new repo in registry with `_name` and publish a first version with contract `_pluginFactoryAddress` and content `@fromHex(_contentURI)`
-     * @param _name Repo name
-     * @param _initialSemanticVersion Semantic version for new repo version
-     * @param _pluginFactoryAddress address for smart contract logic for version (if set to 0, it uses last versions' contractAddress)
-     * @param _contentURI External URI for fetching new version's content
-     */
+    /// @notice Create new repo in registry with `_name` and publish a first version with contract `_pluginFactoryAddress` and content `@fromHex(_contentURI)`
+    /// @param _name Repo name
+    /// @param _initialSemanticVersion Semantic version for new repo version
+    /// @param _pluginFactoryAddress address for smart contract logic for version (if set to 0, it uses last versions' contractAddress)
+    /// @param _contentURI External URI for fetching new version's content
     function newRepoWithVersion(
         string calldata _name,
         uint16[3] memory _initialSemanticVersion,
@@ -44,6 +43,9 @@ contract RepoFactory {
         setRepoPermissions(repo, msg.sender);
     }
 
+    /// @dev Does set the required permissions for the new Repo.
+    /// @param repo The Repo instance just created.
+    /// @param dev The dev's wallet address
     function setRepoPermissions(Repo repo, address dev) internal {
         // set roles on the dao itself.
         ACLData.BulkItem[] memory items = new ACLData.BulkItem[](5);
@@ -64,11 +66,14 @@ contract RepoFactory {
         repo.bulk(address(repo), items);
     }
 
-    function _newRepo(string calldata _name, address initialOwner) internal returns (Repo repo) {
+    /// @dev Does set the required permissions for the new Repo.
+    /// @param _name The Repo instance just created.
+    /// @param _initialOwner The initial owner wallet address
+    function _newRepo(string calldata _name, address _initialOwner) internal returns (Repo repo) {
         if (!(bytes(_name).length > 0)) revert ApmRegEmpityName();
 
         repo = new Repo();
-        repo.initialize(initialOwner);
+        repo.initialize(_initialOwner);
 
         apmRegistry.register(_name, address(repo));
     }
