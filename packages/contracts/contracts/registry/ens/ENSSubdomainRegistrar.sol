c09dcd53701e7e4cb5fc78cd7caf573cecd6081c
Michael Heuer <michael.heuer@rwth-aachen.de>
2022-06-22 21:09:34 +0200

    Fixed forgotten error name

--- packages/contracts/contracts/registry/ens/ENSSubdomainRegistrar.sol
+++ packages/contracts/contracts/registry/ens/ENSSubdomainRegistrar.sol
@@ -47,7 +47,7 @@ contract ENSSubdomainRegistrar is Component {
 
         // This contract must either be the domain node owner or be an approved operator of the node owner
         if (nodeOwner != address(this) && !_ens.isApprovedForAll(nodeOwner, address(this))) {
-            revert RegistrarUnauthorized({nodeOwner: nodeOwner, here: address(this)});
+            revert UnauthorizedRegistrar({nodeOwner: nodeOwner, here: address(this)});
         }
 
         __Component_init(_managingDao);
