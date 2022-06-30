128d22516ac82576b00bce341ebb52d906777564
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2022-06-30 11:40:43 +0200

    update dao registry

--- packages/contracts/deploy/10_registry.ts
+++ packages/contracts/deploy/10_registry.ts
@@ -3,17 +3,27 @@ import {DeployFunction} from 'hardhat-deploy/types';
 import {getContractAddress} from './helpers';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const {deployments, getNamedAccounts} = hre;
+  const {deployments, getNamedAccounts, ethers} = hre;
   const {deploy} = deployments;
 
   const {deployer} = await getNamedAccounts();
 
-  const adminDaoAddress = await getContractAddress('DAO', hre);
-
-  await deploy('Registry', {
+  const ret = await deploy('DAORegistry', {
     from: deployer,
     log: true,
   });
+
+  const managingDAOAddress = await getContractAddress('DAO', hre);
+
+  const registryAddress: string = ret.receipt?.contractAddress || '';
+
+  if (registryAddress !== '') {
+    const registryContract = await ethers.getContractAt(
+      'DAORegistry',
+      registryAddress
+    );
+    await registryContract.initialize(managingDAOAddress);
+  }
 };
 export default func;
-func.tags = ['Registry'];
+func.tags = ['DAORegistry'];
