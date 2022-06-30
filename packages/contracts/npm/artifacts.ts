01bc37a7f3b53bb4ed287f5f9cd4f8b0ff0f234c
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2022-06-30 14:54:32 +0200

    update npm artifact

--- packages/contracts/npm/artifacts.ts
+++ packages/contracts/npm/artifacts.ts
@@ -1,30 +1,30 @@
 // JSON artifacts of the contracts
 
 // Core contracts
-import * as ACL from "../artifacts/contracts/core/acl/ACL.sol/ACL.json";
-import * as ACLData from "../artifacts/contracts/core/acl/ACL.sol/ACLData.json";
-import * as IACLOracle from "../artifacts/contracts/core/acl/IACLOracle.sol/IACLOracle.json";
-import * as DAO from "../artifacts/contracts/core/DAO.sol/DAO.json";
-import * as IDAO from "../artifacts/contracts/core/IDAO.sol/IDAO.json";
-import * as Component from "../artifacts/contracts/core/component/Component.sol/Component.json";
-import * as Permissions from "../artifacts/contracts/core/component/Permissions.sol/Permissions.json";
+import * as ACL from '../artifacts/contracts/core/acl/ACL.sol/ACL.json';
+import * as ACLData from '../artifacts/contracts/core/acl/ACL.sol/ACLData.json';
+import * as IACLOracle from '../artifacts/contracts/core/acl/IACLOracle.sol/IACLOracle.json';
+import * as DAO from '../artifacts/contracts/core/DAO.sol/DAO.json';
+import * as IDAO from '../artifacts/contracts/core/IDAO.sol/IDAO.json';
+import * as Component from '../artifacts/contracts/core/component/Component.sol/Component.json';
+import * as Permissions from '../artifacts/contracts/core/component/Permissions.sol/Permissions.json';
 
 // Factories
-import * as DAOFactory from "../artifacts/contracts/factory/DAOFactory.sol/DAOFactory.json";
-import * as TokenFactory from "../artifacts/contracts/factory/TokenFactory.sol/TokenFactory.json";
+import * as DAOFactory from '../artifacts/contracts/factory/DAOFactory.sol/DAOFactory.json';
+import * as TokenFactory from '../artifacts/contracts/factory/TokenFactory.sol/TokenFactory.json';
 
 // Registry
-import * as Registry from "../artifacts/contracts/registry/Registry.sol/Registry.json";
+import * as Registry from '../artifacts/contracts/registry/DAORegistry.sol/DAORegistry.json';
 
 // Tokens
-import * as GovernanceERC20 from "../artifacts/contracts/tokens/GovernanceERC20.sol/GovernanceERC20.json";
-import * as GovernanceWrappedERC20 from "../artifacts/contracts/tokens/GovernanceWrappedERC20.sol/GovernanceWrappedERC20.json";
-import * as MerkleDistributor from "../artifacts/contracts/tokens/MerkleDistributor.sol/MerkleDistributor.json";
-import * as MerkleMinter from "../artifacts/contracts/tokens/MerkleMinter.sol/MerkleMinter.json";
+import * as GovernanceERC20 from '../artifacts/contracts/tokens/GovernanceERC20.sol/GovernanceERC20.json';
+import * as GovernanceWrappedERC20 from '../artifacts/contracts/tokens/GovernanceWrappedERC20.sol/GovernanceWrappedERC20.json';
+import * as MerkleDistributor from '../artifacts/contracts/tokens/MerkleDistributor.sol/MerkleDistributor.json';
+import * as MerkleMinter from '../artifacts/contracts/tokens/MerkleMinter.sol/MerkleMinter.json';
 
 // Voting (future packages)
-import * as ERC20Voting from "../artifacts/contracts/votings/ERC20/ERC20Voting.sol/ERC20Voting.json";
-import * as WhitelistVoting from "../artifacts/contracts/votings/whitelist/WhitelistVoting.sol/WhitelistVoting.json";
+import * as ERC20Voting from '../artifacts/contracts/votings/ERC20/ERC20Voting.sol/ERC20Voting.json';
+import * as WhitelistVoting from '../artifacts/contracts/votings/whitelist/WhitelistVoting.sol/WhitelistVoting.json';
 
 export default {
   ACL,
