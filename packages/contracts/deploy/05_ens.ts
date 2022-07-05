799af91bf32446bad93f56033dd64ec0378aa642
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2022-07-05 13:51:21 +0200

    Update packages/contracts/deploy/05_ens.ts
    Co-authored-by: Michael Heuer <20623991+Michael-A-Heuer@users.noreply.github.com>

--- packages/contracts/deploy/05_ens.ts
+++ packages/contracts/deploy/05_ens.ts
@@ -33,4 +33,4 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   });
 };
 export default func;
-func.tags = ['ManagingDao'];
+func.tags = ['ENSSubdomainRegistrar'];
