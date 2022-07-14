2a96508c413439b137d49d176518bbcfb31cb0bb
Michael Heuer <michael.heuer@rwth-aachen.de>
2022-07-14 10:23:58 +0200

    Added NatSpec comments to the InterfaceBasedRegistry

--- packages/contracts/contracts/registry/InterfaceBasedRegistry.sol
+++ packages/contracts/contracts/registry/InterfaceBasedRegistry.sol
@@ -11,32 +11,35 @@ import "../core/erc165/AdaptiveERC165.sol";
 /// @author Aragon Association - 2022
 /// @notice An [ERC-165](https://eips.ethereum.org/EIPS/eip-165)-based registry for contracts
 abstract contract InterfaceBasedRegistry is DAOPermissioned, UUPSUpgradeable {
+    /// @notice The ID of the permission required for the `_authorizeUpgrade` function.
     bytes32 public constant UPGRADE_PERMISSION_ID = keccak256("UPGRADE_PERMISSION_ID");
 
+    /// @notice The [ERC-165](https://eips.ethereum.org/EIPS/eip-165) interface ID that the target contracts being registered must support.
     bytes4 public targetInterfaceId;
 
+    /// @notice The mapping containing the registry entries returning true for registererd contract addresses.
     mapping(address => bool) public entries;
 
-    /// @notice Thrown if the contract is already registered
-    /// @param registrant The address of the contract to be registered
+    /// @notice Thrown if the contract is already registered.
+    /// @param registrant The address of the contract to be registered.
     error ContractAlreadyRegistered(address registrant);
 
-    /// @notice Thrown if the contract does not support the required interface
-    /// @param registrant The address of the contract to be registered
+    /// @notice Thrown if the contract does not support the required interface.
+    /// @param registrant The address of the contract to be registered.
     error ContractInterfaceInvalid(address registrant);
 
-    /// @notice Thrown if the address is not a contract
-    /// @param registrant The address of the contract to be registered
+    /// @notice Thrown if the address is not a contract.
+    /// @param registrant The address of the contract to be registered.
     error ContractAddressInvalid(address registrant);
 
-    /// @notice Thrown if the contract do not support ERC165
-    /// @param registrant The address of the contract
+    /// @notice Thrown if the contract do not support ERC165.
+    /// @param registrant The address of the contract.
     error ContractERC165SupportInvalid(address registrant);
 
-    /// @notice Initializes the component
-    /// @dev This is required for the UUPS upgradability pattern
-    /// @param _managingDao The interface of the DAO managing the components permissions
-    /// @param _targetInterfaceId The [ERC-165](https://eips.ethereum.org/EIPS/eip-165) interface id of the contracts to be registered
+    /// @notice Initializes the component.
+    /// @dev This is required for the UUPS upgradability pattern.
+    /// @param _managingDao The interface of the DAO managing the components permissions.
+    /// @param _targetInterfaceId The [ERC-165](https://eips.ethereum.org/EIPS/eip-165) interface id of the contracts to be registered.
     function __InterfaceBasedRegistry_init(IDAO _managingDao, bytes4 _targetInterfaceId)
         internal
         virtual
@@ -51,9 +54,9 @@ abstract contract InterfaceBasedRegistry is DAOPermissioned, UUPSUpgradeable {
     /// @dev The caller must have the `UPGRADE_PERMISSION_ID` permission.
     function _authorizeUpgrade(address) internal virtual override auth(UPGRADE_PERMISSION_ID) {}
 
-    /// @notice Register an [ERC-165](https://eips.ethereum.org/EIPS/eip-165) contract address
-    /// @dev The managing DAO needs to grant REGISTER_PERMISSION_ID to registrar
-    /// @param registrant The address of an [ERC-165](https://eips.ethereum.org/EIPS/eip-165) contract
+    /// @notice Register an [ERC-165](https://eips.ethereum.org/EIPS/eip-165) contract address.
+    /// @dev The managing DAO needs to grant REGISTER_PERMISSION_ID to registrar.
+    /// @param registrant The address of an [ERC-165](https://eips.ethereum.org/EIPS/eip-165) contract.
     function _register(address registrant) internal {
         if (!Address.isContract(registrant)) {
             revert ContractAddressInvalid({registrant: registrant});
