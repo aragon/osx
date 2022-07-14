5f69ab0f5c643c37a121e2e39a677d2d5e99b248
Michael Heuer <michael.heuer@rwth-aachen.de>
2022-07-14 10:02:02 +0200

    Added NatSpec comments to the AragonPluginRegistry

--- packages/contracts/contracts/registry/AragonPluginRegistry.sol
+++ packages/contracts/contracts/registry/AragonPluginRegistry.sol
@@ -8,27 +8,28 @@ import "./InterfaceBasedRegistry.sol";
 import "../core/IDAO.sol";
 import "../plugin/IPluginRepo.sol";
 
-/// @title Register plugin
+/// @title AragonPluginRegistry
 /// @author Aragon Association - 2022
-/// @notice This contract provides the possiblity to register a plugin pluginRepo by a unique address.
+/// @notice This contract maintains an address-based registery of plugin repositories in the Aragon App DAO framework.
 contract AragonPluginRegistry is InterfaceBasedRegistry {
+    /// @notice The ID of the permission required for the `register` function.
     bytes32 public constant REGISTER_PERMISSION_ID = keccak256("REGISTER_PERMISSION_ID");
 
-    /// @notice Emitted if a new PluginRepo is registered
-    /// @param name The name of the PluginRepo
-    /// @param pluginRepo The address of the PluginRepo
+    /// @notice Emitted if a new plugin repository is registered.
+    /// @param name The name of the plugin repository.
+    /// @param pluginRepo The address of the plugin repository.
     event PluginRepoRegistered(string name, address pluginRepo);
 
-    /// @notice Initializes the contract
-    /// @param _dao the managing DAO address
+    /// @notice Initializes the contract by setting calling the `InterfaceBasedRegistry` base class initialize method.
+    /// @param _dao The address of the managing DAO.
     function initialize(IDAO _dao) public initializer {
         bytes4 pluginRepoInterfaceId = type(IPluginRepo).interfaceId;
         __InterfaceBasedRegistry_init(_dao, pluginRepoInterfaceId);
     }
 
-    /// @notice Registers a PluginRepo
-    /// @param name The name of the PluginRepo
-    /// @param registrant The address of the PluginRepo contract
+    /// @notice Registers a plugin repository with a name and address.
+    /// @param name The name of the PluginRepo.
+    /// @param registrant The address of the PluginRepo contract.
     function register(string calldata name, address registrant)
         external
         auth(REGISTER_PERMISSION_ID)
