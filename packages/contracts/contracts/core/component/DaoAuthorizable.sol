3ffba66310b408b76b4080f1574976b0bea1ab3a
Michael Heuer <michael.heuer@rwth-aachen.de>
2022-07-27 16:29:28 +0200

    Modified NatSpec description

--- packages/contracts/contracts/core/component/DaoAuthorizable.sol
+++ packages/contracts/contracts/core/component/DaoAuthorizable.sol
@@ -10,7 +10,7 @@ import "./../IDAO.sol";
 
 /// @title DaoAuthorizable
 /// @author Aragon Association - 2022
-/// @notice An abstract contract providing a meta transaction compatible modifier to make functions permissioned through an associated DAO.
+/// @notice An abstract contract providing a meta transaction compatible modifier to authorize function calls through an associated DAO.
 /// This contract provides an `auth` modifier that can be applied to functions in inheriting contracts. The permission to call these functions is managed by the associated DAO.
 /// @dev Make sure to call `__DaoAuthorizable_init` during initialization of the inheriting contract.
 ///      This contract is compatible with meta transactions through OZ's `ContextUpgradable`.
