75c051a2410aa1f9b8edb387187e618b0e18b44b
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2022-08-11 19:35:40 +0400

    initializable fixed

--- packages/contracts/contracts/core/plugin/AragonPlugin.sol
+++ packages/contracts/contracts/core/plugin/AragonPlugin.sol
@@ -3,15 +3,16 @@
 pragma solidity 0.8.10;
 
 import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol"; 
 
-import "../IDAO.sol";
-import "../permission/PermissionManager.sol";
+import { PermissionManager } from "../permission/PermissionManager.sol";
 
 import "../../utils/AppStorage.sol";
+import "../IDAO.sol";
 
 /// @title AragonPlugin
 /// @notice An abstract contract to inherit from when creating a non-upgradable plugin.
-abstract contract AragonPlugin is AppStorage, Context {
+abstract contract AragonPlugin is Initializable, AppStorage, Context {
 
     /// @dev Auth modifier used in all components of a DAO to check the permissions.
     /// @param _permissionId The hash of the permission identifier
--- packages/contracts/contracts/core/plugin/AragonUpgradablePlugin.sol
+++ packages/contracts/contracts/core/plugin/AragonUpgradablePlugin.sol
@@ -6,10 +6,10 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
 
-import "../IDAO.sol";
-import "../permission/PermissionManager.sol";
+import { PermissionManager } from "../permission/PermissionManager.sol";
 
 import "../../utils/AppStorage.sol";
+import "../IDAO.sol";
 
 /// @title AragonUpgradablePlugin
 /// @notice An Abtract Aragon Plugin(UUPS-UPGRADABLE) that plugin developers have to inherit from.
