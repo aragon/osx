f2156bacb11ff795bd976bcf0eee673fd442a54d
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2022-08-11 18:54:52 +0400

    rename proxy in ts files

--- packages/contracts/deploy/05_ens.ts
+++ packages/contracts/deploy/05_ens.ts
@@ -23,7 +23,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     log: true,
     proxy: {
       owner: deployer,
-      proxyContract: 'PluginERC1967Proxy',
+      proxyContract: 'UUPSProxy',
       execute: {
         init: {
           methodName: 'initialize',
