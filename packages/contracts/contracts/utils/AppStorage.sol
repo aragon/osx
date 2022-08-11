b9c029502282c5538760b9cf60c92b0e934c1cc6
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2022-08-11 18:39:27 +0400

    Update packages/contracts/contracts/utils/AppStorage.sol
    Co-authored-by: Michael Heuer <20623991+Michael-A-Heuer@users.noreply.github.com>

--- packages/contracts/contracts/utils/AppStorage.sol
+++ packages/contracts/contracts/utils/AppStorage.sol
@@ -8,9 +8,8 @@ import "../core/IDAO.sol";
 /// @notice Core storage that stores information at very specific slots.
 contract AppStorage {    
     
-    /* Hardcoded constants to save gas
-    bytes32 internal constant DAO_POSITION = keccak256("core.storage.dao");
-    */
+    /// @notice The position in storage in which the contract address fulfilling the `IDAO` interface is stored
+    /// @dev bytes32 internal constant DAO_POSITION = keccak256("core.storage.dao");
     bytes32 internal constant DAO_POSITION = 0xd69e81f6042b963e91c7595979ec7bb19d41b99e5a44a91c85e5cd5861e49998;
     
     /// @notice Gets the`IDAO` contract being stored in the `DAO_POSITION` storage slot.
