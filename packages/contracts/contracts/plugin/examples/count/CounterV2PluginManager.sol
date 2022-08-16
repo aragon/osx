50233bbd3a94cc02a59a755a2653049093dc5b46
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2022-08-16 19:27:42 +0400

    Update packages/contracts/contracts/plugin/examples/count/CounterV2PluginManager.sol
    Co-authored-by: Jør∂¡ <4456749+brickpop@users.noreply.github.com>

--- packages/contracts/contracts/plugin/examples/count/CounterV2PluginManager.sol
+++ packages/contracts/contracts/plugin/examples/count/CounterV2PluginManager.sol
@@ -37,7 +37,7 @@ contract CounterV2PluginManager is PluginManager {
             _multiplyHelper = createProxy(dao, address(multiplyHelperBase), "0x");
         }
 
-        bytes memory init = abi.encodeWithSelector(
+        bytes memory initData = abi.encodeWithSelector(
             bytes4(keccak256("initialize(address,uint256)")),
             _multiplyHelper,
             _num,
