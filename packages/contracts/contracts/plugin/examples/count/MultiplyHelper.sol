8954cc4d673b4c7c8b0c876e1f589c80d7ebdfb0
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2022-08-16 19:51:14 +0400

    Update packages/contracts/contracts/plugin/examples/count/MultiplyHelper.sol
    Co-authored-by: Michael Heuer <20623991+Michael-A-Heuer@users.noreply.github.com>

--- packages/contracts/contracts/plugin/examples/count/MultiplyHelper.sol
+++ packages/contracts/contracts/plugin/examples/count/MultiplyHelper.sol
@@ -7,7 +7,7 @@ import "../../../core/plugin/AragonPlugin.sol";
 // NON-Upgradable
 contract MultiplyHelper is AragonPlugin  {
     
-    bytes32 public constant MULTIPLY_PERMISSION_ID = keccak256("ADDITION_PERMISSION");
+    bytes32 public constant MULTIPLY_PERMISSION_ID = keccak256("MULTIPLY_PERMISSION");
 
     function multiply(uint a, uint b) external auth(MULTIPLY_PERMISSION_ID) returns (uint) {
         return a * b;
