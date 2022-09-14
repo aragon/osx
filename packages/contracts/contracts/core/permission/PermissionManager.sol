4505b24defb019b804ccf7f29d8e91484cf093f1
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2022-09-14 17:46:59 +0400

    remove comment

--- packages/contracts/contracts/core/permission/PermissionManager.sol
+++ packages/contracts/contracts/core/permission/PermissionManager.sol
@@ -341,13 +341,10 @@ contract PermissionManager is Initializable {
         return false;
     }
     
-    /// @notice A modifier to be used to check permissions on a target contract.
+    /// @notice A private function to be used to check permissions on a target contract.
     /// @param _where The address of the target contract for which the permission is required.
     /// @param _permissionId The permission identifier required to call the method this modifier is applied to.
     function _auth(address _where, bytes32 _permissionId) private view {
-        // TODO: will we need to emit `who` as well ? below `who: msg.sender` 
-        // is not actul who that we grant the permission in the end, it's the sender
-        // that initiations permission.
         if (
             !(isGranted(_where, msg.sender, _permissionId, msg.data) ||
                 isGranted(address(this), msg.sender, _permissionId, msg.data))
