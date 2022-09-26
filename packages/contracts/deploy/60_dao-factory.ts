1d076893035c749a6ac0abefea404c183c085e59
Michael Heuer <michael.heuer@rwth-aachen.de>
2022-09-26 17:22:20 +0200

    Fixed wrong formatting according to our .prettierrc specs.

--- packages/contracts/deploy/60_dao-factory.ts
+++ packages/contracts/deploy/60_dao-factory.ts
@@ -1,13 +1,13 @@
-import { HardhatRuntimeEnvironment } from 'hardhat/types';
-import { DeployFunction } from 'hardhat-deploy/types';
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {DeployFunction} from 'hardhat-deploy/types';
 
-import { getContractAddress } from './helpers';
+import {getContractAddress} from './helpers';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts, ethers } = hre;
-  const { deploy } = deployments;
+  const {deployments, getNamedAccounts, ethers} = hre;
+  const {deploy} = deployments;
 
-  const { deployer } = await getNamedAccounts();
+  const {deployer} = await getNamedAccounts();
 
   const daoRegistryAddress = await getContractAddress('DAORegistry', hre);
   const tokenFactoryAddress = await getContractAddress('TokenFactory', hre);
