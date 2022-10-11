183df68d36be7d6495c07775d5376596a148a475
Michael Heuer <michael.heuer@rwth-aachen.de>
2022-10-11 16:35:00 +0200

    Fixed broken comments

--- packages/contracts/contracts/core/component/CallbackHandler.sol
+++ packages/contracts/contracts/core/component/CallbackHandler.sol
@@ -7,7 +7,7 @@ pragma solidity 0.8.10;
 /// @notice This contract handles callbacks by registering a magic number together with the callback function's selector. It provides the `_handleCallback` function that inherting have to call inside their `fallback()` function  (`_handleCallback(msg.callbackSelector, msg.data)`).  This allows to adaptively register ERC standards (e.g., [ERC-721](https://eips.ethereum.org/EIPS/eip-721), [ERC-1115](https://eips.ethereum.org/EIPS/eip-1155), or future versions of [ERC-165](https://eips.ethereum.org/EIPS/eip-165)) and returning the required magic numbers for the associated callback functions for the inheriting contract so that it doesn't need to be upgraded.
 /// @dev This callback handling functionality is intented to be used by executor contracts (i.e., `DAO.sol`).
 contract CallbackHandler {
-    /// @notice A mapping between function callbackSelectornatures and magic return numbers.
+    /// @notice A mapping between callback function selectors and magic return numbers.
     mapping(bytes4 => bytes32) internal callbackMagicNumbers;
 
     /// @notice The magic number refering to unregistered callbacks.
@@ -18,7 +18,7 @@ contract CallbackHandler {
     /// @param magicNumber The magic number to be registered for the callback function selector.
     error UnkownCallback(bytes4 callbackSelector, bytes32 magicNumber);
 
-    /// @notice Handles callbacks to adaptively support ERC standards
+    /// @notice Handles callbacks to adaptively support ERC standards.
     /// @param _callbackSelector The selector of the callback function.
     /// @dev This function is supposed to be called via `_handleCallback(msg.sig, msg.data)` in the `fallback()` function of the inheriting contract.
     function _handleCallback(bytes4 _callbackSelector) internal view {
@@ -33,9 +33,9 @@ contract CallbackHandler {
         }
     }
 
-    /// @notice Registers a magic number for a callback function callbackSelectornature.
+    /// @notice Registers a magic number for a callback function selector.
     /// @param _callbackSelector The selector of the callback function.
-    /// @param _magicNumber The magic number to be registered for the function callbackSelectornature.
+    /// @param _magicNumber The magic number to be registered for the callback function selector.
     function _registerCallback(bytes4 _callbackSelector, bytes4 _magicNumber) internal {
         callbackMagicNumbers[_callbackSelector] = _magicNumber;
     }
