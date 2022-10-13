9ed1dfc3a1061e74820ff72ac37b5ecef30ded9a
Michael Heuer <michael.heuer@rwth-aachen.de>
2022-10-13 16:08:38 +0200

    Addressed review comment

--- packages/contracts/contracts/core/component/DaoAuthorizableBase.sol
+++ packages/contracts/contracts/core/component/DaoAuthorizableBase.sol
@@ -7,18 +7,12 @@ import "@openzeppelin/contracts/utils/Context.sol";
 import {_auth} from "../../utils/auth.sol";
 import {IDAO} from "../IDAO.sol";
 
-/// @title DaoAuthorizable
+/// @title DaoAuthorizableBase
 /// @author Aragon Association - 2022
-/// @notice An abstract contract providing a meta-transaction compatible modifier for contracts being generated with `new` to authorize function calls through an associated DAO.
-abstract contract DaoAuthorizable is Context {
+/// @notice An abstract contract providing a meta transaction compatible modifier to authorize function calls through an associated DAO.
+abstract contract DaoAuthorizableBase is Context {
     /// @notice The associated DAO managing the permissions of inheriting contracts.
     IDAO internal dao;
-
-    /// @notice Constructs the contract by setting the associated DAO.
-    /// @param _dao The associated DAO address.
-    constructor(IDAO _dao) {
-        dao = _dao;
-    }
 
     /// @notice Returns the DAO contract.
     /// @return IDAO The DAO contract.
--- packages/contracts/contracts/core/component/DaoAuthorizableUpgradeable.sol
+++ packages/contracts/contracts/core/component/DaoAuthorizableUpgradeable.sol
@@ -4,35 +4,46 @@ pragma solidity 0.8.10;
 
 import "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
 
-import {_auth} from "../../utils/auth.sol";
-import "../IDAO.sol";
+import {IDAO} from "../IDAO.sol";
+import {DaoAuthorizableBase} from "./DaoAuthorizableBase.sol";
 
 /// @title DaoAuthorizableUpgradeable
 /// @author Aragon Association - 2022
 /// @notice An abstract contract providing a meta-transaction compatible modifier for upgradeable contracts to authorize function calls through an associated DAO.
 /// @dev Make sure to call `__DaoAuthorizableUpgradeable_init` during initialization of the inheriting contract.
-abstract contract DaoAuthorizableUpgradeable is Initializable, ContextUpgradeable {
-    /// @notice The associated DAO managing the permissions of inheriting contracts.
-    IDAO internal dao;
-
+abstract contract DaoAuthorizableUpgradeable is
+    DaoAuthorizableBase,
+    Initializable,
+    ContextUpgradeable
+{
     /// @notice Initializes the contract by setting the associated DAO.
     /// @param _dao The associated DAO address.
     function __DaoAuthorizableUpgradeable_init(IDAO _dao) internal onlyInitializing {
         dao = _dao;
     }
 
-    /// @notice Returns the DAO contract.
-    /// @return IDAO The DAO contract.
-    function getDAO() external view returns (IDAO) {
-        return dao;
+    /// @inheritdoc ContextUpgradeable
+    function _msgSender()
+        internal
+        view
+        virtual
+        override(Context, ContextUpgradeable)
+        returns (address)
+    {
+        return ContextUpgradeable._msgSender();
     }
 
-    /// @notice A modifier to be used to check permissions on a target contract via the associated DAO.
-    /// @param _permissionId The permission identifier required to call the method this modifier is applied to.
-    modifier auth(bytes32 _permissionId) {
-        _auth(dao, address(this), _msgSender(), _permissionId, _msgData());
-        _;
+    /// @inheritdoc ContextUpgradeable
+    function _msgData()
+        internal
+        view
+        virtual
+        override(Context, ContextUpgradeable)
+        returns (bytes calldata)
+    {
+        return ContextUpgradeable._msgData();
     }
 
     /// @dev This empty reserved space is put in place to allow future versions to add new
