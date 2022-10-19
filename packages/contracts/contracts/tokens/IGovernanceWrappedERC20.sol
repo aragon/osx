ea205b6dfc728331bf5eb0827cdd53e0f8cf1456
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2022-10-19 14:22:44 +0300

    Update packages/contracts/contracts/tokens/IGovernanceWrappedERC20.sol
    Co-authored-by: Michael Heuer <20623991+Michael-A-Heuer@users.noreply.github.com>

--- packages/contracts/contracts/tokens/IGovernanceWrappedERC20.sol
+++ packages/contracts/contracts/tokens/IGovernanceWrappedERC20.sol
@@ -7,9 +7,9 @@ import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/
 import {ERC20WrapperUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20WrapperUpgradeable.sol";
 
 interface IGovernanceWrappedERC20 {
-    /// @notice Allow a user to deposit underlying tokens and mint the corresponding number of wrapped tokens.
-    /// @param account the user address where the tokens should be minted.
-    /// @param amount how much will be minted on the account
+    /// @notice Deposits an underlying tokens and mints the corresponding number of wrapped tokens for an receiving address.
+    /// @param account The address receiving the minted, wrapped tokens.
+    /// @param amount The amount of tokens to be  minted.
     function depositFor(address account, uint256 amount) external returns (bool);
 
     /// @notice Allow a user to burn a number of wrapped tokens and withdraw the corresponding number of underlying tokens.
