46d7c4ba0a012d535cd2d87fddc905d8b713aa35
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2022-10-19 14:05:14 +0300

    Update packages/contracts/contracts/voting/majority/MajorityVotingBase.sol
    Co-authored-by: Michael Heuer <20623991+Michael-A-Heuer@users.noreply.github.com>

--- packages/contracts/contracts/voting/majority/MajorityVotingBase.sol
+++ packages/contracts/contracts/voting/majority/MajorityVotingBase.sol
@@ -310,8 +310,6 @@ its ID.
         minDuration = _minDuration;
     }
 
-    /// @dev This empty reserved space is put in place to allow future versions to add new
-    /// variables without shifting down storage in the inheritance chain.
-    /// https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+    /// @notice This empty reserved space is put in place to allow future versions to add new variables without shifting down storage in the inheritance chain (see [OpenZepplins guide about storage gaps](https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps)).
     uint256[47] private __gap;
 }
