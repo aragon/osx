f461f548ee5247c372456f7bbdeea29e44309397
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2022-10-19 16:44:03 +0300

    Update packages/contracts/contracts/tokens/IGovernanceWrappedERC20.sol
    Co-authored-by: Michael Heuer <20623991+Michael-A-Heuer@users.noreply.github.com>

--- packages/contracts/contracts/tokens/IGovernanceWrappedERC20.sol
+++ packages/contracts/contracts/tokens/IGovernanceWrappedERC20.sol
@@ -12,7 +12,7 @@ interface IGovernanceWrappedERC20 {
     /// @param amount The amount of tokens to be  minted.
     function depositFor(address account, uint256 amount) external returns (bool);
 
-    /// @notice Withdraws  an amount of underlying tokens to an receiving address and burns the corresponding number of wrapped tokens.
+    /// @notice Withdraws an amount of underlying tokens to an receiving address and burns the corresponding number of wrapped tokens.
     /// @param account The address receiving the withdrawn, underlying tokens.
     /// @param amount The amount of underlying tokens to be withdrawn.
     function withdrawTo(address account, uint256 amount) external returns (bool);
