5087cfeda144a642f011cfe3bdc8dcd81a3c46bb
Mathias Scherer <mathias@aragon.org>
2022-11-02 15:23:00 +0100

    App 1174 subgraph adapt add contract events registries (#133)
    * fix: prettier solution to recompile subgraph tests
    
    * feat: adds support for PluginRepo and PluginRegistry to subgraph
    
    * feat: adds event from PluginSetupProcessor to subgraph
    
    * feat: marks some entity as immutable in subgraph
    
    * fix: brings back recompiliation of test files
    
    * chore: updates changelog for subgraph
    
    * fix: adds missing dataSources for subgraph to networks
    
    * chore: updates changelog of contracts
    
    * fix: adjust based on pullrequest comments
    
    * feat: deletes rinkeby from subgraph manifest data
    
    * fix: tests for subgraph

---
+++ packages/subgraph/src/plugin/pluginSetupProcessor.ts
@@ -0,0 +1,121 @@
+import {Address, Bytes} from '@graphprotocol/graph-ts';
+import {
+  InstallationApplied,
+  InstallationPrepared,
+  UninstallationApplied,
+  UninstallationPrepared,
+  UpdateApplied,
+  UpdatePrepared
+} from '../../generated/PluginSetupProcessor/PluginSetupProcessor';
+import {Plugin, PluginHelper} from '../../generated/schema';
+
+export function handleInstallationPrepared(event: InstallationPrepared): void {
+  let pluginId = event.params.plugin.toHexString();
+
+  let pluginEntity = new Plugin(pluginId);
+  pluginEntity.sender = event.params.sender.toHexString();
+  pluginEntity.dao = event.params.dao.toHexString();
+  pluginEntity.pluginSetup = event.params.pluginSetup.toHexString();
+  pluginEntity.data = event.params.data;
+  pluginEntity.state = 'InstallationPrepared';
+
+  handleHelperIds(event.params.helpers, pluginId);
+  pluginEntity.save();
+}
+
+export function handleInstallationApplied(event: InstallationApplied): void {
+  let pluginId = event.params.plugin.toHexString();
+  let pluginEntity = Plugin.load(pluginId);
+  if (!pluginEntity) {
+    pluginEntity = createEmptyPlugin(pluginId);
+    pluginEntity.dao = event.params.dao.toHexString();
+  }
+  pluginEntity.state = 'Installed';
+  pluginEntity.save();
+}
+
+export function handleUpdatePrepared(event: UpdatePrepared): void {
+  let pluginId = event.params.plugin.toHexString();
+
+  let pluginEntity = Plugin.load(pluginId);
+  if (!pluginEntity) {
+    pluginEntity = createEmptyPlugin(pluginId);
+  }
+  pluginEntity.sender = event.params.sender.toHexString();
+  pluginEntity.dao = event.params.dao.toHexString();
+  pluginEntity.pluginSetup = event.params.pluginSetup.toHexString();
+  pluginEntity.data = event.params.data;
+  pluginEntity.state = 'UpdatePrepared';
+  pluginEntity.save();
+
+  handleHelperIds(event.params.updatedHelpers, pluginId);
+}
+
+export function handleUpdateApplied(event: UpdateApplied): void {
+  let pluginId = event.params.plugin.toHexString();
+
+  let pluginEntity = Plugin.load(pluginId);
+  if (!pluginEntity) {
+    pluginEntity = createEmptyPlugin(pluginId);
+    pluginEntity.dao = event.params.dao.toHexString();
+  }
+
+  pluginEntity.state = 'Installed';
+  pluginEntity.save();
+}
+
+export function handleUninstallationPrepared(
+  event: UninstallationPrepared
+): void {
+  let pluginId = event.params.plugin.toHexString();
+
+  let pluginEntity = Plugin.load(pluginId);
+  if (!pluginEntity) {
+    pluginEntity = createEmptyPlugin(pluginId);
+  }
+
+  pluginEntity.sender = event.params.sender.toHexString();
+  pluginEntity.dao = event.params.dao.toHexString();
+  pluginEntity.pluginSetup = event.params.pluginSetup.toHexString();
+  pluginEntity.data = event.params.data;
+  pluginEntity.state = 'UninstallPrepared';
+  pluginEntity.save();
+
+  handleHelperIds(event.params.currentHelpers, pluginId);
+}
+
+export function handleUninstallationApplied(
+  event: UninstallationApplied
+): void {
+  let pluginId = event.params.plugin.toHexString();
+
+  let pluginEntity = Plugin.load(pluginId);
+  if (!pluginEntity) {
+    pluginEntity = createEmptyPlugin(pluginId);
+    pluginEntity.dao = event.params.dao.toHexString();
+  }
+
+  pluginEntity.state = 'Uninstalled';
+  pluginEntity.save();
+}
+
+export function handleHelperIds(helperIds: Address[], plugin: string): void {
+  for (let i = 0; i < helperIds.length; ++i) {
+    let helperId = helperIds[i].toHexString();
+    let helper = PluginHelper.load(helperId);
+    if (!helper) {
+      helper = new PluginHelper(helperId);
+      helper.plugin = plugin;
+      helper.save();
+    }
+  }
+}
+
+function createEmptyPlugin(pluginId: string): Plugin {
+  let pluginEntity = new Plugin(pluginId) as Plugin;
+  pluginEntity.sender = '0x0000000000000000000000000000000000000000';
+  pluginEntity.dao = '0x0000000000000000000000000000000000000000';
+  pluginEntity.pluginSetup = '0x0000000000000000000000000000000000000000';
+  pluginEntity.data = Bytes.fromHexString('0x00');
+  return pluginEntity;
+}
--- packages/subgraph/src/registries/daoRegistry.ts
+++ packages/subgraph/src/registries/daoRegistry.ts
@@ -1,6 +1,6 @@
-import {DAORegistered} from '../generated/DAORegistry/DAORegistry';
-import {DaoTemplate} from '../generated/templates';
-import {Dao} from '../generated/schema';
+import {DAORegistered} from '../../generated/DAORegistry/DAORegistry';
+import {DaoTemplate} from '../../generated/templates';
+import {Dao} from '../../generated/schema';
 
 export function handleDAORegistered(event: DAORegistered): void {
   let id = event.params.dao.toHexString(); // use dao address as id, because it should not repeat
