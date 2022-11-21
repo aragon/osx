a3d1570a76d36b4d9fd84608b90bca33e188de3a
Michael Heuer <michael.heuer@rwth-aachen.de>
2022-11-21 12:59:34 +0100

    Fix inheritance incompatibility with OZ contracts

--- packages/contracts/contracts/core/component/dao-authorizable/DaoAuthorizableUpgradeable.sol
+++ packages/contracts/contracts/core/component/dao-authorizable/DaoAuthorizableUpgradeable.sol
@@ -12,11 +12,7 @@ import {DaoAuthorizableBaseUpgradeable} from "./bases/DaoAuthorizableBaseUpgrade
 /// @author Aragon Association - 2022
 /// @notice An abstract contract providing a meta-transaction compatible modifier for upgradeable contracts to authorize function calls through an associated DAO.
 /// @dev Make sure to call `__DaoAuthorizableUpgradeable_init` during initialization of the inheriting contract.
-abstract contract DaoAuthorizableUpgradeable is
-    Initializable,
-    ContextUpgradeable,
-    DaoAuthorizableBaseUpgradeable
-{
+abstract contract DaoAuthorizableUpgradeable is DaoAuthorizableBaseUpgradeable {
     /// @notice Initializes the contract by setting the associated DAO.
     /// @param _dao The associated DAO address.
     function __DaoAuthorizableUpgradeable_init(IDAO _dao) internal onlyInitializing {
