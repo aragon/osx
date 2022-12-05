ded03e9637c47112e6c6335531f371709966f458
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2022-12-05 11:44:06 +0100

    remove log

--- packages/contracts/deploy/30_plugins/00_plugin-setups/20_admin_setup.ts
+++ packages/contracts/deploy/30_plugins/00_plugin-setups/20_admin_setup.ts
@@ -2,8 +2,6 @@ import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  console.log(`\nDeploying plugins.`);
-
   const {deployments, getNamedAccounts} = hre;
   const {deploy} = deployments;
   const {deployer} = await getNamedAccounts();
