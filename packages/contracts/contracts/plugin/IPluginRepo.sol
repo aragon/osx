5956282522374d66329253935c58fff4dbddd012
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2023-01-10 14:12:29 +0400

    add natpec for createVersion

--- packages/contracts/contracts/plugin/IPluginRepo.sol
+++ packages/contracts/contracts/plugin/IPluginRepo.sol
@@ -6,8 +6,12 @@ pragma solidity 0.8.10;
 /// @author Aragon Association - 2022
 /// @notice The interface required for a plugin repository.
 interface IPluginRepo {
+    /// @notice Creates a new version with contract `_pluginSetupAddress` and content `@fromHex(_contentURI)`.
+    /// @param _release the release number.
+    /// @param _pluginSetupAddress The address of the plugin setup contract.
+    /// @param _contentURI External URI where the plugin metadata and subsequent resources can be fetched from
     function createVersion(
-        uint8 _releaseId,
+        uint8 _release,
         address _pluginSetupAddress,
         bytes calldata _contentURI
     ) external;
