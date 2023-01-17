619fb645b1bc0310d00dadd2dd07c5691590d32b
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2023-01-17 14:51:53 +0400

    review comments

--- packages/contracts/contracts/utils/BitMap.sol
+++ packages/contracts/contracts/utils/BitMap.sol
@@ -2,17 +2,17 @@
 
 pragma solidity 0.8.10;
 
-/// @param bitmap the uint256 representation of bits
-/// @param index the index number to check whether 1 or 0 is set.
-/// @return bool returns true whether the bit is set at `index` on `bitmap`
+/// @param bitmap The `uint256` representation of bits.
+/// @param index The index number to check whether 1 or 0 is set.
+/// @return bool Returns `true` whether the bit is set at `index` on `bitmap`.
 function hasBit(uint256 bitmap, uint8 index) pure returns (bool) {
     uint256 bitValue = bitmap & (1 << (index & 0xff));
     return bitValue > 0;
 }
 
-/// @param bitmap the uint256 representation of bits
-/// @param index the index number to set the bit.
-/// @return uint256 returns a new number on which the bit is set at `index`.
+/// @param bitmap The `uint256` representation of bits.
+/// @param index The index number to set the bit.
+/// @return uint256 Returns a new number on which the bit is set at `index`.
 function setBit(uint256 bitmap, uint8 index) pure returns (uint256) {
     return bitmap ^ (1 << (index & 0xff));
 }
