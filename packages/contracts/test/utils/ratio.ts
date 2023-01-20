54b311e81a460f35d17d120b8f5a0740bfc7d5a9
Michael Heuer <michael.heuer@rwth-aachen.de>
2023-01-20 17:39:02 +0100

    Check for overflows

--- packages/contracts/test/utils/ratio.ts
+++ packages/contracts/test/utils/ratio.ts
@@ -33,6 +33,16 @@ describe('Ratio', function () {
       await expect(ratio.applyRatioCeiled(123, RATIO_BASE)).to.not.be.reverted;
     });
 
+    it('reverts for too large values that would cause an overflow', async () => {
+      const tooLargeValue = ethers.constants.MaxUint256.div(RATIO_BASE).add(1);
+      await expect(
+        ratio.applyRatioCeiled(tooLargeValue, RATIO_BASE)
+      ).to.revertedWithPanic(0x11);
+
+      await expect(ratio.applyRatioCeiled(tooLargeValue.sub(1), RATIO_BASE)).to
+        .not.be.reverted;
+    });
+
     it('does not ceil for division without reminder', async () => {
       expect(await ratio.applyRatioCeiled(32, pctToRatio(50))).to.eq(16);
     });
@@ -49,6 +59,16 @@ describe('Ratio', function () {
         .to.revertedWithCustomError(ratio, 'RatioOutOfBounds')
         .withArgs(RATIO_BASE, tooLargeRatio);
       await expect(ratio.applyRatioFloored(123, RATIO_BASE)).to.not.be.reverted;
+    });
+
+    it('reverts for too large values that would cause an overflow', async () => {
+      const tooLargeValue = ethers.constants.MaxUint256.div(RATIO_BASE).add(1);
+      await expect(
+        ratio.applyRatioCeiled(tooLargeValue, RATIO_BASE)
+      ).to.revertedWithPanic(0x11);
+
+      await expect(ratio.applyRatioCeiled(tooLargeValue.sub(1), RATIO_BASE)).to
+        .not.be.reverted;
     });
 
     it('does not floor for division without reminder', async () => {
