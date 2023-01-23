a7512127db4bc2ef24bd0e32af023900b4b439d6
Michael Heuer <20623991+Michael-A-Heuer@users.noreply.github.com>
2023-01-23 19:10:18 +0100

    APP-1650: Test `applyRationCeiled` and `applyRatioFloored` (#211)
    * Added test showing how precision loss affects the execution criteria
    
    * Fixed the code and adapted tests
    
    
    WIP
    
    
    Renaming
    
    * Added parametrized test
    
    
    .
    
    * Fix subgraph
    
    * Renamed PCT_BASE to RATIO_BASE and moved it into a separate file
    
    * Maintained changelogs
    
    * Cleaning
    
    * Fixed failing test
    
    * Added docs and improved NatSpec
    
    * Remove comments
    
    * Fixed typo
    
    Co-authored-by: Jør∂¡ <4456749+brickpop@users.noreply.github.com>
    
    * Reverted wrong formatting
    
    * Fix naming
    
    * Ensure ceiling for division by ratio that have a remainder
    
    * Fixed failing tests
    
    * Fixed comment
    
    * Fixed test
    
    * Improved ratio functions
    
    * Changed type of supportThreshold and minParticipation from uint64 to uin32
    
    * Added comment
    
    * Improved formula documentation
    
    * Simplified the canExecute conditions
    
    * Corrected supportThreshold constraint
    
    * Added tests
    
    * Fixed limit value in error
    
    * Added zero value check in test
    
    * Fixed and clarified tests
    
    * Added more tests
    
    * Added more tests
    
    * Added boundary check to applyRatioCeiled and applyRatioFloored
    
    * Added noVotesWorstCase and improved documentation
    
    * Fixed error return value
    
    * Added clarifying comment
    
    * Improved comments
    
    * Apply suggestions from code review
    
    Co-authored-by: Mathias Scherer <mathias@aragon.org>
    
    * Move participation check outside of if/else
    
    * Fix typos
    
    * Fixed failing test
    
    * Added a ratio test contract and tests
    
    * Check for overflows
    
    * Apply suggestions from code review
    
    Co-authored-by: Jør∂¡ <4456749+brickpop@users.noreply.github.com>
    
    * Removed before statement in test file
    
    * Fixed typos in test
    
    * Fix to the fix
    
    Co-authored-by: Jør∂¡ <4456749+brickpop@users.noreply.github.com>
    Co-authored-by: Mathias Scherer <mathias@aragon.org>

--- packages/contracts/contracts/voting/addresslist/AddresslistVoting.sol
+++ packages/contracts/contracts/voting/addresslist/AddresslistVoting.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.10;
 import {SafeCastUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/math/SafeCastUpgradeable.sol";
 
 import {IDAO} from "../../core/IDAO.sol";
-import {RATIO_BASE, applyRatioCeiled} from "../../utils/Ratio.sol";
+import {RATIO_BASE, _applyRatioCeiled} from "../../utils/Ratio.sol";
 import {MajorityVotingBase} from "../majority/MajorityVotingBase.sol";
 import {IMajorityVoting} from "../majority/IMajorityVoting.sol";
 import {Addresslist} from "./Addresslist.sol";
@@ -108,7 +108,7 @@ contract AddresslistVoting is Addresslist, MajorityVotingBase {
         proposal_.parameters.snapshotBlock = snapshotBlock;
         proposal_.parameters.votingMode = votingMode();
         proposal_.parameters.supportThreshold = supportThreshold();
-        proposal_.parameters.minVotingPower = applyRatioCeiled(
+        proposal_.parameters.minVotingPower = _applyRatioCeiled(
             addresslistLengthAtBlock(snapshotBlock),
             minParticipation()
         );
