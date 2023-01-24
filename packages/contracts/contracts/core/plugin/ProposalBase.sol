972cee4dbc50b1d587d96d6055b7786fa9ecf844
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2023-01-24 14:06:00 +0400

    Update packages/contracts/contracts/core/plugin/ProposalBase.sol
    Co-authored-by: Michael Heuer <20623991+Michael-A-Heuer@users.noreply.github.com>

--- packages/contracts/contracts/core/plugin/ProposalBase.sol
+++ packages/contracts/contracts/core/plugin/ProposalBase.sol
@@ -43,7 +43,7 @@ abstract contract ProposalBase {
     /// @param _metadata The the proposal metadata.
     /// @param _startDate The start date of the proposal in seconds.
     /// @param _endDate The end date of the proposal in seconds.
-    /// @param _allowFailureMap Allows proposal to succeed even if an action reverts. Uses bitmap representation. If the bit at index `x` is 1, the tx succeeds even if the action at `x` failed. Passing 0 will be treated as atomic execution.
+    /// @param _allowFailureMap A bitmap allowing the proposal to succeed, even if individual actions might revert. If the bit at index `i` is 1, the proposal succeeds even if the `i`th action reverts. A failure map value of 0 requires every action to not revert.
     /// @param _actions The actions that will be executed after the proposal passes.
     /// @return proposalId The ID of the proposal.
     function _createProposal(
