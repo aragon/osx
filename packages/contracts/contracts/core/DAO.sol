efd19a5ad7119a9f23b40973f52146449b1cca13
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2023-01-24 16:17:17 +0400

    Update packages/contracts/contracts/core/DAO.sol
    Co-authored-by: Michael Heuer <20623991+Michael-A-Heuer@users.noreply.github.com>

--- packages/contracts/contracts/core/DAO.sol
+++ packages/contracts/contracts/core/DAO.sol
@@ -278,8 +278,8 @@ contract DAO is
     }
 
     /// @notice Fallback to handle future versions of the [ERC-165](https://eips.ethereum.org/EIPS/eip-165) standard.
-    /// @param _input As per the solidity docs, _input is the alias for msg.data.
-    /// @return bytes The magic number registered on the signature selector.
+    /// @param _input An alias being equivalent to `msg.data`. This a feature of the fallback function was introduced with [solidity compiler version 0.7.6](https://github.com/ethereum/solidity/releases/tag/v0.7.6)
+    /// @return bytes The magic number registered for the function selector triggering the fallback.
     fallback(bytes calldata _input) external returns (bytes memory) {
         bytes4 magicNumber = _handleCallback(msg.sig, _input);
         return abi.encode(magicNumber);
