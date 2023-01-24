4211cdaea19ad6fc267a37029fcf37f276d5884c
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2023-01-24 14:09:01 +0400

    Update packages/contracts/contracts/voting/admin/Admin.sol
    Co-authored-by: Michael Heuer <20623991+Michael-A-Heuer@users.noreply.github.com>

--- packages/contracts/contracts/voting/admin/Admin.sol
+++ packages/contracts/contracts/voting/admin/Admin.sol
@@ -43,7 +43,7 @@ contract Admin is PluginCloneable, Proposal {
     /// @notice Creates and executes a new proposal.
     /// @param _metadata The metadata of the proposal.
     /// @param _actions The actions to be executed.
-    /// @param _allowFailureMap Allows proposal to succeed even if an action reverts. Uses bitmap representation. If the bit at index `x` is 1, the tx succeeds even if the action at `x` failed. Passing 0 will be treated as atomic execution.
+    /// @param _allowFailureMap A bitmap allowing the proposal to succeed, even if individual actions might revert. If the bit at index `i` is 1, the proposal succeeds even if the `i`th action reverts. A failure map value of 0 requires every action to not revert.
     function executeProposal(
         bytes calldata _metadata,
         IDAO.Action[] calldata _actions,
