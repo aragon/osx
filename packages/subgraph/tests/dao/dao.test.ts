9b11d80f141da186ddc4e7237d7c690c8e68f2ce
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2023-01-25 16:01:58 +0400

    fix subgraph

--- packages/subgraph/tests/dao/dao.test.ts
+++ packages/subgraph/tests/dao/dao.test.ts
@@ -28,7 +28,6 @@ import {
   getBalanceOf,
   createNewExecutedEvent,
   createDaoEntityState,
-  createNewWithdrawnEvent,
   createTrustedForwarderSetEvent,
   createSignatureValidatorSetEvent,
   createStandardCallbackRegisteredEvent
