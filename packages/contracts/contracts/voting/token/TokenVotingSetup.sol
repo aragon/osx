f5c28a0eef04a2555cdcfd1bbdcba45ce707f3e7
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2023-01-26 20:06:13 +0400

    remove merkleminter/distributor from tokenvotingsetup (#224)
    * remove merkleminter/distributor from tokenvotingsetup
    
    * Removed unused imports
    
    Co-authored-by: Michael Heuer <michael.heuer@rwth-aachen.de>

--- packages/contracts/contracts/voting/token/TokenVotingSetup.sol
+++ packages/contracts/contracts/voting/token/TokenVotingSetup.sol
@@ -16,8 +16,6 @@ import {PluginSetup, IPluginSetup} from "../../plugin/PluginSetup.sol";
 import {GovernanceERC20} from "../../tokens/GovernanceERC20.sol";
 import {GovernanceWrappedERC20} from "../../tokens/GovernanceWrappedERC20.sol";
 import {IGovernanceWrappedERC20} from "../../tokens/IGovernanceWrappedERC20.sol";
-import {MerkleMinter} from "../../tokens/MerkleMinter.sol";
-import {MerkleDistributor} from "../../tokens/MerkleDistributor.sol";
 import {IERC20MintableUpgradeable} from "../../tokens/IERC20MintableUpgradeable.sol";
 import {MajorityVotingBase} from "../majority/MajorityVotingBase.sol";
 import {TokenVoting} from "./TokenVoting.sol";
@@ -41,12 +39,6 @@ contract TokenVotingSetup is PluginSetup {
 
     /// @notice The address of the `GovernanceWrappedERC20` base contract.
     address public immutable governanceWrappedERC20Base;
-
-    /// @notice The address of the `MerkleMinter` base contract.
-    address public immutable merkleMinterBase;
-
-    /// @notice The `MerkleDistributor` base contract used to initialize the `MerkleMinter`.
-    address public immutable distributorBase;
 
     struct TokenSettings {
         address addr;
@@ -73,7 +65,6 @@ contract TokenVotingSetup is PluginSetup {
 
     /// @notice The contract constructor, that deployes the bases.
     constructor() {
-        distributorBase = address(new MerkleDistributor());
         governanceERC20Base = address(
             new GovernanceERC20(
                 IDAO(address(0)),
@@ -85,7 +76,6 @@ contract TokenVotingSetup is PluginSetup {
         governanceWrappedERC20Base = address(
             new GovernanceWrappedERC20(IERC20Upgradeable(address(0)), "", "")
         );
-        merkleMinterBase = address(new MerkleMinter());
         tokenVotingBase = new TokenVoting();
     }
 
