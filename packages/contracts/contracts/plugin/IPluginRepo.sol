83964e541839472aec93b3b16bd4bf467ac29b7e
Michael Heuer <michael.heuer@rwth-aachen.de>
2023-01-26 19:11:53 +0100

    Improved NatSpec for IPluginRepo

--- packages/contracts/contracts/plugin/IPluginRepo.sol
+++ packages/contracts/contracts/plugin/IPluginRepo.sol
@@ -1,15 +1,15 @@
-// SPDX-License-Identifier:    MIT
+// SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.10;
 
 /// @title IPluginRepo
-/// @author Aragon Association - 2022
+/// @author Aragon Association - 2022-2023
 /// @notice The interface required for a plugin repository.
 interface IPluginRepo {
     /// @notice Creates a new version with contract `_pluginSetupAddress` and content `@fromHex(_contentURI)`.
-    /// @param _release the release number.
+    /// @param _release The release ID.
     /// @param _pluginSetupAddress The address of the plugin setup contract.
-    /// @param _contentURI External URI where the plugin metadata and subsequent resources can be fetched from
+    /// @param _contentURI The URI containing the plugin UI components and related information.
     function createVersion(
         uint8 _release,
         address _pluginSetupAddress,
