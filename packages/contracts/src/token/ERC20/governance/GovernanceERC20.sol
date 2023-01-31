18dbb3c95c23bb686b065004b6b1c0ba8a1a4cec
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2023-01-31 18:22:08 +0400

    Update packages/contracts/src/token/ERC20/governance/GovernanceERC20.sol
    Co-authored-by: Michael Heuer <20623991+Michael-A-Heuer@users.noreply.github.com>

--- packages/contracts/src/token/ERC20/governance/GovernanceERC20.sol
+++ packages/contracts/src/token/ERC20/governance/GovernanceERC20.sol
@@ -100,8 +100,6 @@ contract GovernanceERC20 is
         }
     }
 
-    /// @dev This empty reserved space is put in place to allow future versions to add new
-    /// variables without shifting down storage in the inheritance chain.
-    /// https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+   /// @notice This empty reserved space is put in place to allow future versions to add new variables without shifting down storage in the inheritance chain (see [OpenZepplins guide about storage gaps](https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps)).
     uint256[50] private __gap;
 }
