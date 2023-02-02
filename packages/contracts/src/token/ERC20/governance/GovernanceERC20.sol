059b101184d43cf050a655f340f90d6478a63844
Mathias Scherer <scherer.mat@gmail.com>
2023-02-02 17:24:57 +0100

    fix: formatting in GovernanceERC20

--- packages/contracts/src/token/ERC20/governance/GovernanceERC20.sol
+++ packages/contracts/src/token/ERC20/governance/GovernanceERC20.sol
@@ -100,6 +100,6 @@ contract GovernanceERC20 is
         }
     }
 
-   /// @notice This empty reserved space is put in place to allow future versions to add new variables without shifting down storage in the inheritance chain (see [OpenZepplins guide about storage gaps](https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps)).
+    /// @notice This empty reserved space is put in place to allow future versions to add new variables without shifting down storage in the inheritance chain (see [OpenZepplins guide about storage gaps](https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps)).
     uint256[50] private __gap;
 }
