f0eb258f01ae4498bd9876f786b2ef065f20267a
Michael Heuer <michael.heuer@rwth-aachen.de>
2023-02-13 10:36:40 +0100

    Refactored ProposalBase into an interface

--- packages/contracts/src/core/plugin/proposal/ProposalBase.sol
+++ packages/contracts/src/core/plugin/proposal/ProposalBase.sol
@@ -6,8 +6,8 @@ import {IDAO} from "../../dao/IDAO.sol";
 
 /// @title ProposalBase
 /// @author Aragon Association - 2022-2023
-/// @notice An abstract base contract defining the traits and internal functionality to create and execute proposals.
-abstract contract ProposalBase {
+/// @notice An interface to be implemented by DAO plugins that create and execute proposals.
+interface ProposalBase {
     /// @notice Emitted when a proposal is created.
     /// @param proposalId The ID of the proposal.
     /// @param creator  The creator of the proposal.
@@ -33,55 +33,5 @@ abstract contract ProposalBase {
 
     /// @notice Returns the proposal count determining the next proposal ID.
     /// @return The proposal count.
-    function proposalCount() public view virtual returns (uint256);
-
-    /// @notice Creates a proposal ID.
-    /// @return The proposal ID.
-    function createProposalId() internal virtual returns (uint256);
-
-    /// @notice Internal function to create a proposal.
-    /// @param _metadata The the proposal metadata.
-    /// @param _startDate The start date of the proposal in seconds.
-    /// @param _endDate The end date of the proposal in seconds.
-    /// @param _allowFailureMap A bitmap allowing the proposal to succeed, even if individual actions might revert. If the bit at index `i` is 1, the proposal succeeds even if the `i`th action reverts. A failure map value of 0 requires every action to not revert.
-    /// @param _actions The actions that will be executed after the proposal passes.
-    /// @return proposalId The ID of the proposal.
-    function _createProposal(
-        address _creator,
-        bytes calldata _metadata,
-        uint64 _startDate,
-        uint64 _endDate,
-        IDAO.Action[] calldata _actions,
-        uint256 _allowFailureMap
-    ) internal virtual returns (uint256 proposalId) {
-        proposalId = createProposalId();
-
-        emit ProposalCreated({
-            proposalId: proposalId,
-            creator: _creator,
-            metadata: _metadata,
-            startDate: _startDate,
-            endDate: _endDate,
-            actions: _actions,
-            allowFailureMap: _allowFailureMap
-        });
-    }
-
-    /// @notice Internal function to execute a proposal.
-    /// @param _proposalId The ID of the proposal to be executed.
-    /// @param _actions The array of actions to be executed.
-    /// @param _allowFailureMap A bitmap allowing the proposal to succeed, even if individual actions might revert. If the bit at index `i` is 1, the proposal succeeds even if the `i`th action reverts. A failure map value of 0 requires every action to not revert.
-    function _executeProposal(
-        IDAO _dao,
-        uint256 _proposalId,
-        IDAO.Action[] memory _actions,
-        uint256 _allowFailureMap
-    ) internal virtual {
-        (bytes[] memory execResults, ) = _dao.execute(
-            bytes32(_proposalId),
-            _actions,
-            _allowFailureMap
-        );
-        emit ProposalExecuted({proposalId: _proposalId, execResults: execResults});
-    }
+    function proposalCount() external view returns (uint256);
 }
--- packages/contracts/src/core/plugin/proposal/ProposalUpgradeable.sol
+++ packages/contracts/src/core/plugin/proposal/ProposalUpgradeable.sol
@@ -8,7 +8,7 @@ import "./ProposalBase.sol";
 
 /// @title ProposalUgradeable
 /// @author Aragon Association - 2022-2023
-/// @notice An abstract contract defining the traits and internal functionality to create and execute proposals for upgradeable contracts.
+/// @notice An abstract contract to be inherited by upgradeable DAO plugins containing the traits and internal functionality to create and execute proposals.
 abstract contract ProposalUpgradeable is ProposalBase {
     using CountersUpgradeable for CountersUpgradeable.Counter;
 
@@ -20,10 +20,57 @@ abstract contract ProposalUpgradeable is ProposalBase {
         return proposalCounter.current();
     }
 
-    /// @inheritdoc ProposalBase
-    function createProposalId() internal override returns (uint256 proposalId) {
+    /// @notice Creates a proposal ID.
+    /// @return proposalId The proposal ID.
+    function _createProposalId() internal returns (uint256 proposalId) {
         proposalId = proposalCount();
         proposalCounter.increment();
+    }
+
+    /// @notice Internal function to create a proposal.
+    /// @param _metadata The the proposal metadata.
+    /// @param _startDate The start date of the proposal in seconds.
+    /// @param _endDate The end date of the proposal in seconds.
+    /// @param _allowFailureMap A bitmap allowing the proposal to succeed, even if individual actions might revert. If the bit at index `i` is 1, the proposal succeeds even if the `i`th action reverts. A failure map value of 0 requires every action to not revert.
+    /// @param _actions The actions that will be executed after the proposal passes.
+    /// @return proposalId The ID of the proposal.
+    function _createProposal(
+        address _creator,
+        bytes calldata _metadata,
+        uint64 _startDate,
+        uint64 _endDate,
+        IDAO.Action[] calldata _actions,
+        uint256 _allowFailureMap
+    ) internal virtual returns (uint256 proposalId) {
+        proposalId = _createProposalId();
+
+        emit ProposalCreated({
+            proposalId: proposalId,
+            creator: _creator,
+            metadata: _metadata,
+            startDate: _startDate,
+            endDate: _endDate,
+            actions: _actions,
+            allowFailureMap: _allowFailureMap
+        });
+    }
+
+    /// @notice Internal function to execute a proposal.
+    /// @param _proposalId The ID of the proposal to be executed.
+    /// @param _actions The array of actions to be executed.
+    /// @param _allowFailureMap A bitmap allowing the proposal to succeed, even if individual actions might revert. If the bit at index `i` is 1, the proposal succeeds even if the `i`th action reverts. A failure map value of 0 requires every action to not revert.
+    function _executeProposal(
+        IDAO _dao,
+        uint256 _proposalId,
+        IDAO.Action[] memory _actions,
+        uint256 _allowFailureMap
+    ) internal virtual {
+        (bytes[] memory execResults, ) = _dao.execute(
+            bytes32(_proposalId),
+            _actions,
+            _allowFailureMap
+        );
+        emit ProposalExecuted({proposalId: _proposalId, execResults: execResults});
     }
 
     /// @notice This empty reserved space is put in place to allow future versions to add new variables without shifting down storage in the inheritance chain (see [OpenZepplins guide about storage gaps](https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps)).
