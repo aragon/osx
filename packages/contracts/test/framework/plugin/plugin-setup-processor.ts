b7696b62e66a621c367160756c6bec47a246fce6
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2023-02-23 00:33:42 +0400

    remove only

--- packages/contracts/test/framework/plugin/plugin-setup-processor.ts
+++ packages/contracts/test/framework/plugin/plugin-setup-processor.ts
@@ -118,7 +118,7 @@ const REGISTER_ENS_SUBDOMAIN_PERMISSION_ID = ethers.utils.id(
 
 const {UPGRADE_PLUGIN_PERMISSION_ID} = UPGRADE_PERMISSIONS;
 
-describe.only('Plugin Setup Processor', function () {
+describe('Plugin Setup Processor', function () {
   let signers: SignerWithAddress[];
   let psp: PluginSetupProcessor;
   let repoU: PluginRepo;
