0941d9082bf470bd9fead4f83be87c22ac9cb808
Michael Heuer <michael.heuer@rwth-aachen.de>
2023-02-22 10:16:39 +0100

    Fix natspec

--- packages/contracts/src/plugins/governance/admin/AdminSetup.sol
+++ packages/contracts/src/plugins/governance/admin/AdminSetup.sol
@@ -19,14 +19,14 @@ contract AdminSetup is PluginSetup {
     /// @notice The address of `Admin` plugin logic contract to be cloned.
     address private immutable implementation;
 
-    /// @notice The address zero to be used as condition address for permissions.
+    /// @notice An address flag encoding that no conditions are added to a requested permission.
     address private constant NO_CONDITION = address(0);
 
-    /// @notice Thrown if admin address is zero.
+    /// @notice Thrown if the admin address is zero.
     /// @param admin The admin address.
     error AdminAddressInvalid(address admin);
 
-    /// @notice The contract constructor, that deployes the `Admin` plugin logic contract.
+    /// @notice The constructor setting the `Admin` implementation contract to clone from.
     constructor() {
         implementation = address(new Admin());
     }
@@ -75,10 +75,7 @@ contract AdminSetup is PluginSetup {
     }
 
     /// @inheritdoc IPluginSetup
-    /// @dev Currently there is not a relaiable mean to revoke plugin's permissions such as `ADMIN_EXECUTE_PERMISSION_ID`
-    /// that have been granted to addresses during the life cycle of the plugin.
-    /// or the ones that have been granted are not revoked already,
-    /// therefore, only `EXECUTE_PERMISSION_ID` is revoked for this uninstallation.
+    /// @dev Currently, there is no reliable way to revoke the `ADMIN_EXECUTE_PERMISSION_ID` from all addresses it has been granted to. Accordingly, only the `EXECUTE_PERMISSION_ID` is revoked for this uninstallation.
     function prepareUninstallation(
         address _dao,
         SetupPayload calldata _payload
