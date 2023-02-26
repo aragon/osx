3d9fb580eb0d3e7ee727d931f95fb89cc9ed2812
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2023-02-26 23:08:23 +0400

    remove only

--- packages/contracts/test/deploy/managing-dao.ts
+++ packages/contracts/test/deploy/managing-dao.ts
@@ -19,7 +19,7 @@ async function deployAll() {
 const IMPLEMENTATION_SLOT =
   '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc';
 
-describe.only('Managing DAO', function () {
+describe('Managing DAO', function () {
   let ehre: EHRE;
   let ownerAddress: string;
   let managingDaoDeployment: Deployment;
