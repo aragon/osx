38bd7ad244478c526e56c49ef483c56855eda2d0
Mathias Scherer <mathias@aragon.org>
2023-03-01 11:22:37 +0100

    feat: adds blocklist to remove subdomains from indexing (#310)
    * feat: adds blocklist to remove subdomains from indexing
    
    * chore: update changelog and subgraph version
    
    * chore: adds version to changelog

--- packages/subgraph/src/registries/daoRegistry.ts
+++ packages/subgraph/src/registries/daoRegistry.ts
@@ -1,12 +1,22 @@
 import {DAORegistered} from '../../generated/DAORegistry/DAORegistry';
 import {DaoTemplate} from '../../generated/templates';
 import {Dao} from '../../generated/schema';
+import {dataSource} from '@graphprotocol/graph-ts';
+
+// blocklists of addresses for which we don't index the subdomain.
+// Put the reason next to the address as a comment
+const subdomain_blocklist_mainnet = [
+  '0x16070493aa513f91fc8957f14b7b7c6c0c41fbac' // domain squatting lido.dao.eth
+];
 
 export function handleDAORegistered(event: DAORegistered): void {
   let id = event.params.dao.toHexString(); // use dao address as id, because it should not repeat
   let entity = new Dao(id);
 
-  entity.subdomain = event.params.subdomain;
+  if (!isInSubdomainBlocklist(id)) {
+    entity.subdomain = event.params.subdomain;
+  }
+
   entity.creator = event.params.creator;
   entity.createdAt = event.block.timestamp;
 
@@ -14,4 +24,14 @@ export function handleDAORegistered(event: DAORegistered): void {
   DaoTemplate.create(event.params.dao);
 
   entity.save();
+}
+
+// checks if a certain address is in the blocklist. Returns false by default
+function isInSubdomainBlocklist(address: string): bool {
+  // switch case doesn't work with assemblyscript
+  if (dataSource.network() == 'mainnet') {
+    return subdomain_blocklist_mainnet.includes(address);
+  }
+
+  return false;
 }
