3213fed2edbd52e95c2245d495aed5026c98f15e
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2023-03-03 16:30:18 +0100

    create release entity

--- packages/subgraph/src/plugin/pluginRepo.ts
+++ packages/subgraph/src/plugin/pluginRepo.ts
@@ -1,15 +1,32 @@
-import {VersionCreated} from '../../generated/templates/PluginRepoTemplate/PluginRepo';
-import {PluginVersion, PluginSetup} from '../../generated/schema';
+import {
+  ReleaseMetadataUpdated,
+  VersionCreated
+} from '../../generated/templates/PluginRepoTemplate/PluginRepo';
+import {
+  PluginVersion,
+  PluginSetup,
+  PluginRelease
+} from '../../generated/schema';
 
 export function handleVersionCreated(event: VersionCreated): void {
-  let id = `${event.address.toHexString()}_${event.params.release.toString()}_${event.params.build.toString()}`;
+  let pluginRepoId = event.address.toHexString();
   let pluginSetupId = event.params.pluginSetup.toHexString();
-  let entity = new PluginVersion(id);
+  let pluginRelease = event.params.release.toString();
+
+  let pluginVersionId = pluginRepoId
+    .concat('_')
+    .concat(pluginRelease)
+    .concat('_')
+    .concat(event.params.build.toString());
+
+  let pluginReleaseId = pluginRepoId.concat('_').concat(pluginRelease);
+
+  let entity = new PluginVersion(pluginVersionId);
   entity.pluginRepo = event.address.toHexString();
-  entity.release = event.params.release;
+  entity.release = pluginReleaseId;
   entity.build = event.params.build;
   entity.pluginSetup = pluginSetupId;
-  entity.metadata = event.params.buildMetadata;
+  entity.metadata = event.params.buildMetadata.toString();
   entity.save();
 
   let pluginSetupEntity = PluginSetup.load(pluginSetupId);
@@ -17,4 +34,26 @@ export function handleVersionCreated(event: VersionCreated): void {
     pluginSetupEntity = new PluginSetup(pluginSetupId);
     pluginSetupEntity.save();
   }
+}
+
+export function handleReleaseMetadataUpdated(
+  event: ReleaseMetadataUpdated
+): void {
+  let pluginRepoId = event.address.toHexString();
+  let pluginRelease = event.params.release;
+  let releaseMetadata = event.params.releaseMetadata.toString();
+
+  let pluginReleaseEntityId = pluginRepoId
+    .concat('_')
+    .concat(pluginRelease.toString());
+
+  let pluginReleaseEntity = PluginRelease.load(pluginReleaseEntityId);
+  if (!pluginReleaseEntity) {
+    pluginReleaseEntity = new PluginRelease(pluginReleaseEntityId);
+    pluginReleaseEntity.pluginRepo = pluginRepoId;
+    pluginReleaseEntity.release = pluginRelease;
+  }
+
+  pluginReleaseEntity.metadata = releaseMetadata;
+  pluginReleaseEntity.save();
 }
