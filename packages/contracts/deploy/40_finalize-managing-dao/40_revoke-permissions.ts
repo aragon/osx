d52cf75c8c48153fde95f24653e6500420e52a9d
Mathias Scherer <mathias@aragon.org>
2023-03-13 17:01:46 +0100

    fix: fixes ENS setup deployment

--- packages/contracts/deploy/40_finalize-managing-dao/40_revoke-permissions.ts
+++ packages/contracts/deploy/40_finalize-managing-dao/40_revoke-permissions.ts
@@ -73,4 +73,4 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   );
 };
 export default func;
-func.tags = ['RegisterManagingDAO'];
+func.tags = ['RevokeManagingPermissionsDAO'];
