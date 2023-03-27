153becf4fdd3ddef10badd6c91f4b91cd9ae81e8
Mathias Scherer <mathias@aragon.org>
2023-03-27 20:04:04 +0200

    feat: improves deployment script to work properly with polygon

--- packages/contracts/deploy/30_plugins/00_plugin-setups/31_multisig_setup_conclude.ts
+++ packages/contracts/deploy/30_plugins/00_plugin-setups/31_multisig_setup_conclude.ts
@@ -1,18 +1,25 @@
 import {DeployFunction} from 'hardhat-deploy/types';
 import {MultisigSetup__factory} from '../../../typechain';
 import {EHRE} from '../../../utils/types';
+import {setTimeout} from 'timers/promises';
 
 const func: DeployFunction = async function (hre: EHRE) {
   console.log(`Concluding multisig setup deployment.\n`);
   const [deployer] = await hre.ethers.getSigners();
 
-  const {deployments} = hre;
+  const {deployments, network} = hre;
 
   const MultisigSetupDeployment = await deployments.get('MultisigSetup');
   const multisigSetup = MultisigSetup__factory.connect(
     MultisigSetupDeployment.address,
     deployer
   );
+
+  // add a timeout for polygon because the call to `implementation()` can fail for newly deployed contracts in the first few seconds
+  if (network.name === 'polygon') {
+    console.log(`Waiting 30secs for ${network.name} to finish up...`)
+    await setTimeout(30000);
+  }
 
   hre.aragonToVerifyContracts.push(MultisigSetupDeployment);
   hre.aragonToVerifyContracts.push({
