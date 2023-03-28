696a7b923478d66a64767f736e941d952c6f5c31
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2023-03-28 16:26:52 +0300

    implement generator

---
+++ packages/subgraph/scripts/methdos/newMothod.ts
@@ -0,0 +1,4 @@
+export function newMethod() {
+  const message = 'This is a new method in the generated class!';
+  console.log(message);
+}
---
+++ packages/subgraph/tests/helpers/builders/token/tokenVotingVoter.ts
@@ -0,0 +1,34 @@
+import {Address, BigInt} from '@graphprotocol/graph-ts';
+import {assert} from 'matchstick-as';
+import {TokenVotingVoter} from '../../../../generated/schema';
+import {ADDRESS_ONE, CONTRACT_ADDRESS, ZERO} from '../../../constants';
+
+export class TokenVotingVoterBuild extends TokenVotingVoter {
+  constructor() {
+    super(ADDRESS_ONE);
+    this.address = ADDRESS_ONE;
+    this.plugin = CONTRACT_ADDRESS;
+    this.lastUpdated = BigInt.fromString(ZERO);
+  }
+
+  buildOrUpdate() {
+    this.save();
+  }
+
+  assertEntity(): void {
+    let entity = TokenVotingVoter.load(this.id);
+    if (!entity) throw new Error(`Entity not found for id: ${this.id}`);
+
+    let entries = entity.entries;
+    for (let i = 0; i < entries.length; i++) {
+      let key = entries[i].key;
+      let value = this.get(key)?.toString();
+
+      if (!value) {
+        throw new Error(`value is null for key: ${key}`);
+      }
+
+      assert.fieldEquals('TokenVotingVoter', this.id, key, value);
+    }
+  }
+}
