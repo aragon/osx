5e7a19ec2c25723701be07d2757a72ee2fb06866
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2023-05-02 15:26:42 +0200

    F/ OS-380 npm package versions (#361)
    * prepare package
    
    * remember the branch
    
    * only artifact src
    
    * correct the paths
    
    * correct root path
    
    * update hashes
    
    * add logs
    
    * only copy typechain
    
    * generate typechain
    
    * setup rollup
    
    * update npm packages
    
    * update rollup index
    
    * add esm cjs
    
    * add readme
    
    * remove gitIgnore
    
    * update typechain
    
    * update typescript
    
    * convert script to typescript
    
    * change commit hash json
    
    * rename types to typechain
    
    * add test
    
    * add json to rollup and export types
    
    * update usage test
    
    * generate index.ts dynamically
    
    * update readme
    
    * add empty line
    
    * fix index.ts
    
    * ci/cd for tests
    
    * fix-typo
    
    * remove check formatting
    
    * add checkout ref
    
    * update contracts flow
    
    * update contracts workflow
    
    * update contracts prettier config
    
    * remove directories
    
    * remove more dir

---
+++ packages/contracts-versions/README.md
@@ -0,0 +1,51 @@
+# Aragon OSx Contracts Versions
+
+A package to manage different contract versions and provide easy access to their ABI, types, and active contracts.
+
+## Installation
+
+```bash
+npm install @aragon/osx-versions
+```
+
+or
+
+```bash
+yarn add @aragon/osx-versions
+```
+
+## Usage
+
+```javascript
+// import specific version
+import {v0_7_0_alpha_active_contracts, v0_7_0_alpha_typechain} from '@aragon/osx-versions';
+
+const typechain = v0_7_0_alpha_typechain;
+const idao: v0_7_0_alpha_typechain.IDAO = typechain.IDAO__factory.connect(
+  ethers.constants.AddressZero,
+  ethers.providers.getDefaultProvider()
+);
+```
+
+## Adding new contract versions
+
+1. Update `commit_hashes.json` with the new version name and the associated commit hash.
+2. Run the `create-contract-versions.ts` script to build and generate the new version:
+
+```bash
+yarn build:contracts
+```
+
+3. Run the Rollup build process:
+
+```bash
+yarn build:npm
+```
+
+## Contributing
+
+Contributions are welcome! Feel free to open a pull request or create an issue to report bugs or request features.
+
+## License
+
+This project is licensed under the AGPL-3.0-or-later License.
---
+++ packages/contracts-versions/create-contract-versions.ts
@@ -0,0 +1,103 @@
+const fs = require('fs-extra');
+const path = require('path');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+const monorepoRoot = path.join(__dirname, '../..');
+const contractsDir = path.join(monorepoRoot, 'packages/contracts');
+const contractVersionsDir = path.join(__dirname, 'build');
+const commitHashes = require('./commit_hashes.json');
+
+async function getCurrentBranch() {
+  const {stdout} = await exec('git branch --show-current', {cwd: contractsDir});
+  return stdout.trim();
+}
+
+async function buildContracts(commit: string) {
+  try {
+    await exec(`git checkout ${commit}`, {cwd: contractsDir});
+    await exec('yarn build', {cwd: contractsDir});
+  } catch (error) {
+    console.error('Error building contracts:', error);
+  }
+}
+
+async function copyActiveContracts(commit: string, versionName: string) {
+  try {
+    console.log(`Copying active_contracts.json`);
+    const srcActiveContracts = path.join(monorepoRoot, 'active_contracts.json');
+    const destActiveContracts = path.join(
+      contractVersionsDir,
+      versionName,
+      'active_contracts.json'
+    );
+    await fs.copy(srcActiveContracts, destActiveContracts);
+  } catch (error) {
+    console.error('Error copying active contracts:', error);
+  }
+}
+
+async function generateTypechain(src: string, dest: string) {
+  try {
+    // Find all the .json files, excluding the .dbg.json files, in all subdirectories
+    const {stdout} = await exec(
+      `find "${src}" -name '*.json' -type f -not -path '*.dbg.json'`
+    );
+    const jsonFiles = stdout
+      .trim()
+      .split('\n')
+      .map((file: string) => `"${file}"`) // Added type annotation here
+      .join(' ');
+
+    // Run typechain for all .json files at once
+    await exec(`typechain --target ethers-v5 --out-dir "${dest}" ${jsonFiles}`);
+  } catch (error) {
+    console.error('Error generating TypeChain output:', error);
+  }
+}
+
+async function createVersions() {
+  const currentBranch = await getCurrentBranch();
+
+  for (const version in commitHashes.versions) {
+    const versionCommit = commitHashes.versions[version] as string;
+    const versionName = version;
+
+    console.log(
+      `Building contracts for version: ${versionName}, with commit: ${versionCommit}`
+    );
+    await buildContracts(versionCommit);
+    await copyActiveContracts(versionCommit, versionName);
+
+    const srcArtifacts = path.join(contractsDir, 'artifacts/src');
+    const destTypechain = path.join(
+      contractVersionsDir,
+      versionName,
+      'typechain'
+    );
+    await generateTypechain(srcArtifacts, destTypechain);
+  }
+
+  // Return to the original branch
+  await exec(`git checkout ${currentBranch}`, {cwd: contractsDir});
+
+  // Generate npm/index.ts file
+  const exports: string[] = [];
+  for (const version in commitHashes.versions) {
+    const versionName = version;
+    exports.push(
+      `export * as ${versionName}_typechain from 'build/${versionName}/typechain';`
+    );
+    exports.push(
+      `import * as ${versionName}_active_contracts from 'build/${versionName}/active_contracts.json';`
+    );
+  }
+  exports.push(
+    `export { ${Object.keys(commitHashes.versions)
+      .map(versionName => `${versionName}_active_contracts`)
+      .join(', ')} };`
+  );
+  await fs.writeFile(path.join(__dirname, 'index.ts'), exports.join('\n'));
+}
+
+createVersions();
