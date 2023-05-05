3e25be2b4f5fad6ecd01f373d20862c2068d57ba
Michael Heuer <michael.heuer@rwth-aachen.de>
2023-05-05 10:19:16 +0200

    add legacy versions

--- packages/contracts/test/upgrade/dao.ts
+++ packages/contracts/test/upgrade/dao.ts
@@ -2,11 +2,13 @@ import {expect} from 'chai';
 import {ethers} from 'hardhat';
 import {SignerWithAddress} from '@nomiclabs/hardhat-ethers/signers';
 
-import {DAO__factory} from '../../typechain';
 import {
-  v1_0_0_mainnet_goerli_typechain,
-  v1_0_0_mumbai_typechain,
-} from '@aragon/osx-versions';
+  DAO__factory,
+  DAOV100,
+  DAOV100__factory,
+  DAOV110Alpha,
+  DAOV110Alpha__factory,
+} from '../../typechain';
 
 import {daoExampleURI} from '../test-utils/dao';
 import {deployWithProxy} from '../test-utils/proxy';
@@ -15,8 +17,8 @@ import {findEventTopicLog} from '../../utils/event';
 import {readImplementationValueFromSlot} from '../../utils/storage';
 
 let signers: SignerWithAddress[];
-let Dao_mainnet_goerli_V1_0_0: v1_0_0_mainnet_goerli_typechain.DAO__factory;
-let Dao_mumbai_V1_0_0: v1_0_0_mumbai_typechain.DAO__factory;
+let Dao_v1_0_0: DAOV100__factory;
+let Dao_v1_1_0_alpha: DAOV110Alpha__factory;
 let DaoCurrent: DAO__factory;
 
 const DUMMY_METADATA = ethers.utils.hexlify(
@@ -26,17 +28,14 @@ const DUMMY_METADATA = ethers.utils.hexlify(
 describe('DAO Upgrade', function () {
   before(async function () {
     signers = await ethers.getSigners();
-    Dao_mainnet_goerli_V1_0_0 =
-      new v1_0_0_mainnet_goerli_typechain.DAO__factory(signers[0]);
-    Dao_mumbai_V1_0_0 = new v1_0_0_mumbai_typechain.DAO__factory(signers[0]);
+    Dao_v1_0_0 = new DAOV100__factory(signers[0]);
+    Dao_v1_1_0_alpha = new DAOV110Alpha__factory(signers[0]);
 
     DaoCurrent = new DAO__factory(signers[0]);
   });
 
-  it('upgrades mainnet/goerli v1.0.0 to v1.1.0', async () => {
-    const proxy = await deployWithProxy<v1_0_0_mainnet_goerli_typechain.DAO>(
-      Dao_mainnet_goerli_V1_0_0
-    );
+  it('upgrades v1.0.0 to v1.1.0', async () => {
+    const proxy = await deployWithProxy<DAOV100>(Dao_v1_0_0);
     await proxy.initialize(
       DUMMY_METADATA,
       signers[0].address,
@@ -72,11 +71,7 @@ describe('DAO Upgrade', function () {
 
     // Check the emitted implementation.
     const emittedImplementation = (
-      await findEventTopicLog(
-        upgradeTx,
-        Dao_mainnet_goerli_V1_0_0.interface,
-        'Upgraded'
-      )
+      await findEventTopicLog(upgradeTx, Dao_v1_0_0.interface, 'Upgraded')
     ).args.implementation;
     expect(emittedImplementation).to.equal(newImplementation.address);
 
@@ -84,10 +79,8 @@ describe('DAO Upgrade', function () {
     expect(await proxy.callStatic.daoURI()).to.equal(daoExampleURI);
   });
 
-  it('upgrades mumbai v1.0.0 to v1.1.0', async () => {
-    const proxy = await deployWithProxy<v1_0_0_mumbai_typechain.DAO>(
-      Dao_mumbai_V1_0_0
-    );
+  it('upgrades v1.0.0-alpha (mumbai pre-release) to v1.1.0', async () => {
+    const proxy = await deployWithProxy<DAOV110Alpha>(Dao_v1_1_0_alpha);
     await proxy.initialize(
       DUMMY_METADATA,
       signers[0].address,
@@ -123,11 +116,7 @@ describe('DAO Upgrade', function () {
 
     // Check the emitted implementation.
     const emittedImplementation = (
-      await findEventTopicLog(
-        upgradeTx,
-        Dao_mumbai_V1_0_0.interface,
-        'Upgraded'
-      )
+      await findEventTopicLog(upgradeTx, Dao_v1_1_0_alpha.interface, 'Upgraded')
     ).args.implementation;
     expect(emittedImplementation).to.equal(newImplementation.address);
 
