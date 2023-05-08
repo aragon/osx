6cb94caf7f0ca1024ed624fcfa20c9acca5eaa29
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2023-05-08 17:06:25 +0200

    update contract versions

--- packages/contracts-versions/create-contract-versions.ts
+++ packages/contracts-versions/create-contract-versions.ts
@@ -1,5 +1,6 @@
 const fs = require('fs-extra');
 const path = require('path');
+const glob = require('glob');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
@@ -22,8 +23,17 @@ async function buildContracts(commit: string) {
   }
 }
 
-async function copyActiveContracts(commit: string, versionName: string) {
+async function copyActiveContracts(versionName: string) {
   try {
+    console.log(`Copying contracts source code`);
+    const srcContracts = path.join(contractsDir, 'src');
+    const destContracts = path.join(
+      contractVersionsDir,
+      versionName,
+      'contracts'
+    );
+    await fs.copy(srcContracts, destContracts);
+
     console.log(`Copying active_contracts.json`);
     const srcActiveContracts = path.join(monorepoRoot, 'active_contracts.json');
     const destActiveContracts = path.join(
@@ -33,7 +43,10 @@ async function copyActiveContracts(commit: string, versionName: string) {
     );
     await fs.copy(srcActiveContracts, destActiveContracts);
   } catch (error) {
-    console.error('Error copying active contracts:', error);
+    console.error(
+      'Error copying contracts source code and active contracts:',
+      error
+    );
   }
 }
 
@@ -67,7 +80,7 @@ async function createVersions() {
       `Building contracts for version: ${versionName}, with commit: ${versionCommit}`
     );
     await buildContracts(versionCommit);
-    await copyActiveContracts(versionCommit, versionName);
+    await copyActiveContracts(versionName);
 
     const srcArtifacts = path.join(contractsDir, 'artifacts/src');
     const destTypechain = path.join(
@@ -86,10 +99,10 @@ async function createVersions() {
   for (const version in commitHashes.versions) {
     const versionName = version;
     exports.push(
-      `export * as ${versionName}_typechain from 'build/${versionName}/typechain';`
+      `export * as ${versionName}_typechain from '../build/${versionName}/typechain';`
     );
     exports.push(
-      `import * as ${versionName}_active_contracts from 'build/${versionName}/active_contracts.json';`
+      `import * as ${versionName}_active_contracts from '../build/${versionName}/active_contracts.json';`
     );
   }
   exports.push(
@@ -97,7 +110,10 @@ async function createVersions() {
       .map(versionName => `${versionName}_active_contracts`)
       .join(', ')} };`
   );
-  await fs.writeFile(path.join(__dirname, 'index.ts'), exports.join('\n'));
+
+  const npmDir = path.join(__dirname, 'npm');
+  await fs.ensureDir(npmDir);
+  await fs.writeFile(path.join(npmDir, 'index.ts'), exports.join('\n'));
 }
 
 createVersions();
--- packages/contracts-versions/rollup.config.ts
+++ packages/contracts-versions/rollup.config.ts
@@ -1,34 +1,40 @@
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import json from '@rollup/plugin-json';
+import copy from 'rollup-plugin-copy';
 
 export default [
   {
-    input: 'index.ts',
-    plugins: [typescript({project: './tsconfig.json'}), json()],
+    input: 'npm/index.ts',
+    plugins: [
+      typescript({project: './tsconfig.json'}),
+      json(),
+      copy({
+        targets: [{src: 'build/*', dest: 'dist/build'}],
+      }),
+    ],
     output: [
       {
         dir: 'dist',
         entryFileNames: 'index-esm.js',
         format: 'esm',
         exports: 'named',
-        chunkFileNames: 'chunks/[name]-[hash].js',
+        sourcemap: true,
       },
       {
         dir: 'dist',
         entryFileNames: 'index-cjs.js',
         format: 'cjs',
         exports: 'named',
-        chunkFileNames: 'chunks/[name]-[hash].js',
+        sourcemap: true,
       },
     ],
   },
   {
-    input: 'index.ts',
+    input: 'npm/index.ts',
     plugins: [dts(), json()],
     output: {
-      dir: 'dist',
-      entryFileNames: 'bundle.d.ts',
+      file: 'dist/bundle.d.ts',
       format: 'es',
     },
   },
