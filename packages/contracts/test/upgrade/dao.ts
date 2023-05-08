9b6a514af80df2c082ecf2a7aea2f6eac2ef278a
Michael Heuer <michael.heuer@rwth-aachen.de>
2023-05-08 10:55:58 +0200

    feat: added more tests

--- packages/contracts/test/upgrade/dao.ts
+++ packages/contracts/test/upgrade/dao.ts
@@ -3,6 +3,7 @@ import {ethers} from 'hardhat';
 import {SignerWithAddress} from '@nomiclabs/hardhat-ethers/signers';
 
 import {
+  DAO,
   DAO__factory,
   DAOV101,
   DAOV101__factory,
@@ -21,6 +22,15 @@ let Dao_v1_0_1: DAOV101__factory;
 let Dao_v1_2_0: DAOV120__factory;
 let DaoCurrent: DAO__factory;
 
+let daoV101Proxy: DAOV101;
+let daoV120Proxy: DAOV120;
+
+let daoV101Implementation: string;
+let daoV120Implementation: string;
+let daoCurrentImplementaion: DAO;
+
+const EMPTY_DATA = '0x';
+
 const DUMMY_METADATA = ethers.utils.hexlify(
   ethers.utils.toUtf8Bytes('0x123456789')
 );
@@ -32,95 +42,190 @@ describe('DAO Upgrade', function () {
     Dao_v1_2_0 = new DAOV120__factory(signers[0]);
 
     DaoCurrent = new DAO__factory(signers[0]); // 1.3.0
+
+    // Deploy the v1.3.0 implementation
+    daoCurrentImplementaion = await DaoCurrent.deploy();
   });
 
-  it('upgrades v1.0.1 to v1.3.0', async () => {
-    const proxy = await deployWithProxy<DAOV101>(Dao_v1_0_1);
-    await proxy.initialize(
-      DUMMY_METADATA,
-      signers[0].address,
-      ethers.constants.AddressZero,
-      daoExampleURI
-    );
+  context(`v1.0.1 to v1.3.0`, function () {
+    beforeEach(async function () {
+      daoV101Proxy = await deployWithProxy<DAOV101>(Dao_v1_0_1);
+      await daoV101Proxy.initialize(
+        DUMMY_METADATA,
+        signers[0].address,
+        ethers.constants.AddressZero,
+        daoExampleURI
+      );
 
-    // Store the current implementation
-    const implementationBeforeUpgrade = await readImplementationValueFromSlot(
-      proxy.address
-    );
+      // Store the v1.0.1 implementation
+      daoV101Implementation = await readImplementationValueFromSlot(
+        daoV101Proxy.address
+      );
 
-    proxy.grant(
-      proxy.address,
-      signers[0].address,
-      UPGRADE_PERMISSIONS.UPGRADE_DAO_PERMISSION_ID
-    );
+      // Grant the upgrade permission
+      await daoV101Proxy.grant(
+        daoV101Proxy.address,
+        signers[0].address,
+        UPGRADE_PERMISSIONS.UPGRADE_DAO_PERMISSION_ID
+      );
+    });
 
-    // Deploy the new implementation
-    const newImplementation = await DaoCurrent.deploy();
+    it('does not corrupt the DAO storage', async () => {
+      // Upgrade to the new implementation
+      const upgradeTx = await daoV101Proxy.upgradeTo(
+        daoCurrentImplementaion.address
+      );
 
-    // Upgrade to the new implementation
-    const upgradeTx = await proxy.upgradeTo(newImplementation.address);
+      // Check the stored implementation.
+      const implementationAfterUpgrade = await readImplementationValueFromSlot(
+        daoV101Proxy.address
+      );
+      expect(implementationAfterUpgrade).to.equal(
+        daoCurrentImplementaion.address
+      );
+      expect(implementationAfterUpgrade).to.not.equal(daoV101Implementation);
 
-    // Check the stored implementation.
-    const implementationAfterUpgrade = await readImplementationValueFromSlot(
-      proxy.address
-    );
-    expect(implementationAfterUpgrade).to.equal(newImplementation.address);
-    expect(implementationAfterUpgrade).to.not.equal(
-      implementationBeforeUpgrade
-    );
+      // Check the emitted implementation.
+      const emittedImplementation = (
+        await findEventTopicLog(upgradeTx, Dao_v1_0_1.interface, 'Upgraded')
+      ).args.implementation;
+      expect(emittedImplementation).to.equal(daoCurrentImplementaion.address);
 
-    // Check the emitted implementation.
-    const emittedImplementation = (
-      await findEventTopicLog(upgradeTx, Dao_v1_0_1.interface, 'Upgraded')
-    ).args.implementation;
-    expect(emittedImplementation).to.equal(newImplementation.address);
+      // Check that storage is not corrupted.
+      expect(await daoV101Proxy.callStatic.daoURI()).to.equal(daoExampleURI);
+    });
 
-    // Check that storage is not corrupted.
-    expect(await proxy.callStatic.daoURI()).to.equal(daoExampleURI);
+    it('does not corrupt permissions', async () => {
+      await daoV101Proxy.grant(
+        daoV101Proxy.address,
+        signers[0].address,
+        ethers.utils.id('EXECUTE_PERMISSION')
+      );
+      await daoV101Proxy.grant(
+        daoV101Proxy.address,
+        signers[0].address,
+        ethers.utils.id('ROOT_PERMISSION')
+      );
+
+      // Upgrade to the new implementation
+      await daoV101Proxy.upgradeTo(daoCurrentImplementaion.address);
+
+      // Check the stored implementation.
+      const implementationAfterUpgrade = await readImplementationValueFromSlot(
+        daoV101Proxy.address
+      );
+      expect(implementationAfterUpgrade).to.equal(
+        daoCurrentImplementaion.address
+      );
+      expect(implementationAfterUpgrade).to.not.equal(daoV101Implementation);
+
+      expect(
+        await daoV101Proxy.hasPermission(
+          daoV101Proxy.address,
+          signers[0].address,
+          ethers.utils.id('EXECUTE_PERMISSION'),
+          EMPTY_DATA
+        )
+      ).to.be.true;
+      expect(
+        await daoV101Proxy.hasPermission(
+          daoV101Proxy.address,
+          signers[0].address,
+          ethers.utils.id('ROOT_PERMISSION'),
+          EMPTY_DATA
+        )
+      ).to.be.true;
+    });
   });
 
-  it('upgrades v1.2.0 to v1.3.0', async () => {
-    const proxy = await deployWithProxy<DAOV120>(Dao_v1_2_0);
-    await proxy.initialize(
-      DUMMY_METADATA,
-      signers[0].address,
-      ethers.constants.AddressZero,
-      daoExampleURI
-    );
+  context(`v1.2.0 to v1.3.0`, function () {
+    beforeEach(async function () {
+      daoV120Proxy = await deployWithProxy<DAOV120>(Dao_v1_2_0);
+      await daoV120Proxy.initialize(
+        DUMMY_METADATA,
+        signers[0].address,
+        ethers.constants.AddressZero,
+        daoExampleURI
+      );
 
-    // Store the current implementation
-    const implementationBeforeUpgrade = await readImplementationValueFromSlot(
-      proxy.address
-    );
+      // Store the v1.2.0 implementation
+      daoV120Implementation = await readImplementationValueFromSlot(
+        daoV120Proxy.address
+      );
 
-    proxy.grant(
-      proxy.address,
-      signers[0].address,
-      UPGRADE_PERMISSIONS.UPGRADE_DAO_PERMISSION_ID
-    );
+      // Grant the upgrade permission
+      await daoV120Proxy.grant(
+        daoV120Proxy.address,
+        signers[0].address,
+        UPGRADE_PERMISSIONS.UPGRADE_DAO_PERMISSION_ID
+      );
+    });
 
-    // Deploy the new implementation
-    const newImplementation = await DaoCurrent.deploy();
+    it('does not corrupt the DAO storage', async () => {
+      // Upgrade to the new implementation
+      const upgradeTx = await daoV120Proxy.upgradeTo(
+        daoCurrentImplementaion.address
+      );
 
-    // Upgrade to the new implementation
-    const upgradeTx = await proxy.upgradeTo(newImplementation.address);
+      // Check the stored implementation.
+      const implementationAfterUpgrade = await readImplementationValueFromSlot(
+        daoV120Proxy.address
+      );
+      expect(implementationAfterUpgrade).to.equal(
+        daoCurrentImplementaion.address
+      );
+      expect(implementationAfterUpgrade).to.not.equal(daoV120Implementation);
 
-    // Check the stored implementation.
-    const implementationAfterUpgrade = await readImplementationValueFromSlot(
-      proxy.address
-    );
-    expect(implementationAfterUpgrade).to.equal(newImplementation.address);
-    expect(implementationAfterUpgrade).to.not.equal(
-      implementationBeforeUpgrade
-    );
+      // Check the emitted implementation.
+      const emittedImplementation = (
+        await findEventTopicLog(upgradeTx, Dao_v1_2_0.interface, 'Upgraded')
+      ).args.implementation;
+      expect(emittedImplementation).to.equal(daoCurrentImplementaion.address);
 
-    // Check the emitted implementation.
-    const emittedImplementation = (
-      await findEventTopicLog(upgradeTx, Dao_v1_2_0.interface, 'Upgraded')
-    ).args.implementation;
-    expect(emittedImplementation).to.equal(newImplementation.address);
+      // Check that storage is not corrupted.
+      expect(await daoV120Proxy.callStatic.daoURI()).to.equal(daoExampleURI);
+    });
 
-    // Check that storage is not corrupted.
-    expect(await proxy.callStatic.daoURI()).to.equal(daoExampleURI);
+    it('does not corrupt permissions', async () => {
+      await daoV120Proxy.grant(
+        daoV120Proxy.address,
+        signers[0].address,
+        ethers.utils.id('EXECUTE_PERMISSION')
+      );
+      await daoV120Proxy.grant(
+        daoV120Proxy.address,
+        signers[0].address,
+        ethers.utils.id('ROOT_PERMISSION')
+      );
+
+      // Upgrade to the new implementation
+      await daoV120Proxy.upgradeTo(daoCurrentImplementaion.address);
+
+      // Check the stored implementation.
+      const implementationAfterUpgrade = await readImplementationValueFromSlot(
+        daoV120Proxy.address
+      );
+      expect(implementationAfterUpgrade).to.equal(
+        daoCurrentImplementaion.address
+      );
+      expect(implementationAfterUpgrade).to.not.equal(daoV120Implementation);
+
+      expect(
+        await daoV120Proxy.hasPermission(
+          daoV120Proxy.address,
+          signers[0].address,
+          ethers.utils.id('EXECUTE_PERMISSION'),
+          EMPTY_DATA
+        )
+      ).to.be.true;
+      expect(
+        await daoV120Proxy.hasPermission(
+          daoV120Proxy.address,
+          signers[0].address,
+          ethers.utils.id('ROOT_PERMISSION'),
+          EMPTY_DATA
+        )
+      ).to.be.true;
+    });
   });
 });
