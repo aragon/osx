f437dbc32e0f0c6b816d82295c4a353385ac2b19
Michael Heuer <michael.heuer@rwth-aachen.de>
2023-05-08 09:45:29 +0200

    doc: clarified the origin of the implementation slot

--- packages/contracts/test/test-utils/uups-upgradeable.ts
+++ packages/contracts/test/test-utils/uups-upgradeable.ts
@@ -4,8 +4,9 @@ import {Contract} from 'ethers';
 import {defaultAbiCoder} from 'ethers/lib/utils';
 import {ethers} from 'hardhat';
 
+// See https://eips.ethereum.org/EIPS/eip-1967
 export const IMPLEMENTATION_SLOT =
-  '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc';
+  '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'; // bytes32(uint256(keccak256('eip1967.proxy.implementation')) - 1)
 
 /// Used as a common test suite to test upgradeability of the contracts.
 /// Presumes that `upgrade` object is set on `this` inside the actual test file.
