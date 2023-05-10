cde0c14bc2c6fc0b25029a116c5b00e67b3899d0
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2023-05-10 12:45:10 +0200

    fix test

--- packages/contracts-versions/test/usage.test.ts
+++ packages/contracts-versions/test/usage.test.ts
@@ -1,11 +1,11 @@
 import {
-  v1_0_1_active_contracts,
+  v1_0_0_active_contracts,
   v1_2_0_active_contracts,
 } from '@aragon/osx-versions';
 
 describe('contract-versions', () => {
   it('should get active contracts for a specific version', async () => {
-    const activeContracts101 = v1_0_1_active_contracts;
+    const activeContracts101 = v1_0_0_active_contracts;
     expect(activeContracts101).toBeDefined();
 
     const activeContracts120 = v1_2_0_active_contracts;
@@ -13,7 +13,7 @@ describe('contract-versions', () => {
   });
 
   it('should not have mumbai deployment', async () => {
-    const activeContracts101: Record<string, any> = v1_0_1_active_contracts;
+    const activeContracts101: Record<string, any> = v1_0_0_active_contracts;
     expect(activeContracts101['mumbai']).toBeUndefined();
   });
 
