8a4941b07a49b8ac7e87b6b0f568af1cabcc5008
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2023-05-30 13:04:06 +0200

    address comments

--- packages/contracts/test/deploy/updateTo1_3_0.ts
+++ packages/contracts/test/deploy/updateTo1_3_0.ts
@@ -5,13 +5,12 @@ import {initForkAndFixture} from '../test-utils/fixture';
 import {v1_0_0_active_contracts} from '@aragon/osx-versions';
 
 const enableTest = process.env.TEST_UPDATE_DEPLOY_SCRIPT !== undefined;
+if (enableTest) {
+  describe('update/to_v1.3.0', function () {
+    before(async () => {
+      await initForkAndFixture('mainnet', 'v1_3_0', 'v1_0_0');
+    });
 
-describe('update/to_v1.3.0', function () {
-  before(async () => {
-    await initForkAndFixture('mainnet', 'v1_3_0', 'v1_0_0');
-  });
-
-  if (enableTest) {
     it('deploys new contracts with new addresses', async function () {
       const previousDAOFactory = v1_0_0_active_contracts.mainnet.DAOFactory;
       const previousMultisigSetup =
@@ -32,5 +31,5 @@ describe('update/to_v1.3.0', function () {
         previousMultisigSetup
       );
     });
-  }
-});
+  });
+}
