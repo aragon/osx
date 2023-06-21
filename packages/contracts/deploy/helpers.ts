ba10e5d0693518b7c1d0f8af2cbba2172e12949b
Jose Manuel Mariñas Bascoy <josemarinasbascoy@gmail.com>
2023-06-21 12:50:53 +0200

    add ipfs api key

--- packages/contracts/deploy/helpers.ts
+++ packages/contracts/deploy/helpers.ts
@@ -59,7 +59,7 @@ export async function uploadToIPFS(
   const client = IPFS.create({
     url: 'https://prod.ipfs.aragon.network/api/v0',
     headers: {
-      'X-API-KEY': 'b477RhECf8s8sdM7XrkLBs2wHc4kCMwpbcFC55Kt',
+      'X-API-KEY': process.env.IPFS_API_KEY ?? '',
     },
   });
 
