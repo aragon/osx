4bfa839045ccd229d97a94afee9c063c0e4519ce
Mathias Scherer <mathias@aragon.org>
2023-06-23 11:16:48 +0200

    fix: includes plugin bases in contract verification

--- packages/contracts/deploy/update/to_v1.3.0/42_TokenVoting_Plugin_conclude.ts
+++ packages/contracts/deploy/update/to_v1.3.0/42_TokenVoting_Plugin_conclude.ts
@@ -1,9 +1,19 @@
 import {DeployFunction} from 'hardhat-deploy/types';
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {UPDATE_INFOS} from '../../../utils/updates';
+import {TokenVotingSetup__factory} from '../../../typechain';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   console.log('\nConcluding TokenVoting Plugin Update');
+  const [deployer] = await hre.ethers.getSigners();
+
+  const TokenVotingSetupDeployment = await hre.deployments.get(
+    'TokenVotingSetup'
+  );
+  const tokenVotingSetup = TokenVotingSetup__factory.connect(
+    TokenVotingSetupDeployment.address,
+    deployer
+  );
 
   hre.aragonToVerifyContracts.push(
     await hre.deployments.get('GovernanceERC20')
@@ -13,9 +23,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     await hre.deployments.get('GovernanceWrappedERC20')
   );
 
-  hre.aragonToVerifyContracts.push(
-    await hre.deployments.get('TokenVotingSetup')
-  );
+  hre.aragonToVerifyContracts.push(TokenVotingSetupDeployment);
+  hre.aragonToVerifyContracts.push({
+    address: await tokenVotingSetup.implementation(),
+    args: [],
+  });
+
+  hre.aragonToVerifyContracts.push();
 };
 export default func;
 func.tags = ['TokenVotingPlugin', 'Verify'].concat(UPDATE_INFOS['v1_3_0'].tags);
