cd7c28688efacf8c395fe8e59ecc000f6b14dd40
Mathias Scherer <mathias@aragon.org>
2023-07-11 13:01:18 +0200

    R/1.3.0 rc0 (#429)
    * chore: updates contracts changelog with all the changes
    
    * chore: fixes typos in UPDATE_CHECKLIST.md
    
    * feat(rc0): managingDAO deploy proposal is now json
    
    * fix(rc0): error in update checklist copying wrong env variables
    
    * Provide Governance Token Bases to `TokenVotingSetup` Constructor as Arguments to Avoid `initCode` size limit on Goerli (#425)
    
    * fix: provide governance token bases as constructor arguments
    
    * feat: verify governance token bases
    
    * fix: adapt script for fresh deployment
    
    * fix: typo
    
    * fix: includes plugin bases in contract verification
    
    * chore(1.3.0): update active_contract.json with new addresses of testnets
    chore(1.3.0): update Releases.md with new releases in testnets
    
    * chore: update npm package versions to 1.3.0-rc0
    
    ---------
    
    Co-authored-by: Michael Heuer <20623991+Michael-A-Heuer@users.noreply.github.com>

--- packages/contracts/deploy/update/to_v1.3.0/32_Multisig_Plugin_conclude.ts
+++ packages/contracts/deploy/update/to_v1.3.0/32_Multisig_Plugin_conclude.ts
@@ -1,11 +1,23 @@
 import {DeployFunction} from 'hardhat-deploy/types';
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {UPDATE_INFOS} from '../../../utils/updates';
+import {MultisigSetup__factory} from '../../../typechain';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   console.log('\nConcluding Multisig Plugin Update');
+  const [deployer] = await hre.ethers.getSigners();
 
-  hre.aragonToVerifyContracts.push(await hre.deployments.get('MultisigSetup'));
+  const MultisigSetupDeployment = await hre.deployments.get('MultisigSetup');
+  const multisigSetup = MultisigSetup__factory.connect(
+    MultisigSetupDeployment.address,
+    deployer
+  );
+
+  hre.aragonToVerifyContracts.push(MultisigSetupDeployment);
+  hre.aragonToVerifyContracts.push({
+    address: await multisigSetup.implementation(),
+    args: [],
+  });
 };
 export default func;
 func.tags = ['MultisigPlugin', 'Verify'].concat(UPDATE_INFOS['v1_3_0'].tags);
--- packages/contracts/deploy/update/to_v1.3.0/52_AddresslistVoting_Plugin_conclude.ts
+++ packages/contracts/deploy/update/to_v1.3.0/52_AddresslistVoting_Plugin_conclude.ts
@@ -1,13 +1,25 @@
 import {DeployFunction} from 'hardhat-deploy/types';
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {UPDATE_INFOS} from '../../../utils/updates';
+import {AddresslistVotingSetup__factory} from '../../../typechain';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   console.log('\nConcluding AddresslistVoting Plugin Update');
+  const [deployer] = await hre.ethers.getSigners();
 
-  hre.aragonToVerifyContracts.push(
-    await hre.deployments.get('AddresslistVotingSetup')
+  const AddresslistVotingSetupDeployment = await hre.deployments.get(
+    'AddresslistVotingSetup'
   );
+  const addresslistVotingSetup = AddresslistVotingSetup__factory.connect(
+    AddresslistVotingSetupDeployment.address,
+    deployer
+  );
+
+  hre.aragonToVerifyContracts.push(AddresslistVotingSetupDeployment);
+  hre.aragonToVerifyContracts.push({
+    address: await addresslistVotingSetup.implementation(),
+    args: [],
+  });
 };
 export default func;
 func.tags = ['AddresslistVotingPlugin', 'Verify'].concat(
