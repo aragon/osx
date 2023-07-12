31dd66741a9d8981ada590319aee14efae16232e
Jose Manuel Mariñas Bascoy <josemarinasbascoy@gmail.com>
2023-07-12 12:08:59 +0200

    add export of build metadata on contracts-ethers

--- packages/contracts-ethers/npm/index.ts
+++ packages/contracts-ethers/npm/index.ts
@@ -1,4 +1,12 @@
 import activeContracts from '../../../active_contracts.json';
+import addresslistVotingMetadata from '../../contracts/src/plugins/governance/majority-voting/addresslist/build-metadata.json';
+import tokenVotingMetadata from '../../contracts/src/plugins/governance/majority-voting/token/build-metadata.json';
+import multisigMetadata from '../../contracts/src/plugins/governance/multisig/build-metadata.json';
+import adminMetadata from '../../contracts/src/plugins/governance/admin/build-metadata.json';
 
 export * from '../types/';
 export const activeContractsList = activeContracts;
+export const addresslistVotingBuildMetadata = addresslistVotingMetadata;
+export const tokenVotingBuildMetadata = tokenVotingMetadata;
+export const multisigBuildMetadata = multisigMetadata;
+export const adminBuildMetadata = adminMetadata;
