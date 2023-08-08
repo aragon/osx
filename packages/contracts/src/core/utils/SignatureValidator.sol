84578cc45872f94c56883019648680cf41c5942e
Michael Heuer <michael.heuer@rwth-aachen.de>
2023-08-08 20:05:59 +0200

    docs: added NatSpec and minor optimizations

--- packages/contracts/src/core/utils/SignatureValidator.sol
+++ packages/contracts/src/core/utils/SignatureValidator.sol
@@ -6,32 +6,45 @@ import {IERC1271} from "@openzeppelin/contracts/interfaces/IERC1271.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import {IDAO} from "../dao/IDAO.sol";
 
-/// @notice An ERC-1271 signature validator implementation to be used by Aragon OSx DAO.
+/// @title SignatureValidator
+/// @author Aragon Association - 2023
+/// @notice An [ERC-1271](https://eips.ethereum.org/EIPS/eip-1271) signature validator implementation to be used by multiple Aragon OSx DAOs.
+/// @dev This contract assumes that incoming calls happen from DAOs having set this contract via the `setSignatureValidator(address)` function in `DAO.sol`.
 contract SignatureValidator is IERC1271 {
     using ECDSA for bytes32;
 
-    /// @notice The ID of the permission required to call the `execute` function.
+    /// @notice The ID of the permission required for the signer signature to be accepted during execution time.
     bytes32 public constant SIGN_PERMISSION_ID = keccak256("SIGN_PERMISSION");
 
-    bytes4 internal constant VALID_EIP1271_SIGNATURE = 0x1626ba7e; // type(IERC1271).interfaceId = bytes4(keccak256("isValidSignature(bytes32,bytes)")
+    /// @notice The internal constant storing the magic return value of a valid [ERC-1271](https://eips.ethereum.org/EIPS/eip-1271) signature.
+    bytes4 internal constant VALID_SIGNATURE = 0x1626ba7e; // type(IERC1271).interfaceId = bytes4(keccak256("isValidSignature(bytes32,bytes)")
+
+    /// @notice The internal constant storing the return value of an invalid [ERC-1271](https://eips.ethereum.org/EIPS/eip-1271) signature.
     bytes4 internal constant INVALID_SIGNATURE = 0xffffffff; // bytes4(uint32(type(uint32).max-1))
 
+    /// @notice Returns whether the signature provided is valid for the provided data.
+    /// @param _hash The hash of the data to be signed.
+    /// @param _signature The signature byte array associated with hash.
+    /// @dev For the signature to be valid, the `SIGN_PERMISSION_ID` permission must be granted to the signer on the calling DAO during execution time.
     function isValidSignature(
-        bytes32 _messageHash,
+        bytes32 _hash,
         bytes calldata _signature
     ) external view returns (bytes4) {
-        address dao = msg.sender; // This holds because DAO has set the signature validator itself via `setSignatureValidator` and calls it via `isValidSignature`.
-        address signer = _messageHash.recover(_signature);
+        // We assume that this contract is an external call is relayed from the `isValidSignature(bytes32, bytes)` function of a DAO,
+        // which has set this contract via the `setSignatureValidator(address)`.
+        address dao = msg.sender;
 
-        bool isValid = IDAO(dao).hasPermission({
-            _where: dao,
-            _who: signer,
-            _permissionId: SIGN_PERMISSION_ID,
-            _data: ""
-        });
+        address signer = _hash.recover(_signature);
 
-        if (isValid) {
-            return VALID_EIP1271_SIGNATURE;
+        if (
+            IDAO(dao).hasPermission({
+                _where: dao,
+                _who: signer,
+                _permissionId: SIGN_PERMISSION_ID,
+                _data: ""
+            })
+        ) {
+            return VALID_SIGNATURE;
         } else {
             return INVALID_SIGNATURE;
         }
--- packages/contracts/test/core/dao/signature-validator.ts
+++ packages/contracts/test/core/dao/signature-validator.ts
@@ -151,6 +151,7 @@ describe('SignatureValidator', function () {
       });
 
       it.skip('eth_signTypedData_v4 (with unused types)', async () => {
+        await dao.grant(dao.address, signer.address, SIGN_PERMISSION_ID);
         const rawTypedData =
           '{"types":{"EIP712Domain":[{"name":"name","type":"string"},{"name":"version","type":"string"},{"name":"chainId","type":"uint256"},{"name":"verifyingContract","type":"address"}],"Person":[{"name":"name","type":"string"},{"name":"wallet","type":"address"}],"Mail":[{"name":"from","type":"Person"},{"name":"to","type":"Person"},{"name":"contents","type":"string"}]},"primaryType":"Mail","domain":{"name":"Ether Mail","version":"1","chainId":1,"verifyingContract":"0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC"},"message":{"from":{"name":"Cow","wallet":"0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826"},"to":{"name":"Bob","wallet":"0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB"},"contents":"Hello, Bob!"}}';
         const typedData = JSON.parse(rawTypedData);
