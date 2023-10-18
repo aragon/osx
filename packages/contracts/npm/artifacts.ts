1e86945db102eb83ce3b300f8895ed7af7258bda
Michael Heuer <michael.heuer@rwth-aachen.de>
2023-10-18 11:04:51 +0200

    refactor: removed TokenFactory

--- packages/contracts/npm/artifacts.ts
+++ packages/contracts/npm/artifacts.ts
@@ -12,7 +12,6 @@ import * as PermissionLib from '../artifacts/src/core/permission/PermissionLib.s
 //// dao
 import * as DAOFactory from '../artifacts/src/framework/dao/DAOFactory.sol/DAOFactory.json';
 import * as DAORegistry from '../artifacts/src/framework/dao/DAORegistry.sol/DAORegistry.json';
-import * as TokenFactory from '../artifacts/src/framework/utils/TokenFactory.sol/TokenFactory.json';
 
 //// Plugin
 ///// Repo
@@ -58,7 +57,6 @@ export default {
   PermissionLib,
 
   DAOFactory,
-  TokenFactory,
   DAORegistry,
 
   PluginRepo,
