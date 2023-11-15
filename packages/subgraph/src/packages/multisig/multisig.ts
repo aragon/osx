13558cc999783c37fd4ff4269e3d0afb1443373c
Michael Heuer <michael.heuer@rwth-aachen.de>
2023-11-15 18:35:22 +0100

    refactor: renamed local variable

--- packages/subgraph/src/packages/multisig/multisig.ts
+++ packages/subgraph/src/packages/multisig/multisig.ts
@@ -46,20 +46,20 @@ export function _handleProposalCreated(
   proposalEntity.allowFailureMap = event.params.allowFailureMap;
 
   let contract = Multisig.bind(event.address);
-  let vote = contract.try_getProposal(pluginProposalId);
+  let proposal = contract.try_getProposal(pluginProposalId);
 
-  if (!vote.reverted) {
-    proposalEntity.executed = vote.value.value0;
-    proposalEntity.approvals = vote.value.value1;
+  if (!proposal.reverted) {
+    proposalEntity.executed = proposal.value.value0;
+    proposalEntity.approvals = proposal.value.value1;
 
     // ProposalParameters
-    let parameters = vote.value.value2;
+    let parameters = proposal.value.value2;
     proposalEntity.minApprovals = parameters.minApprovals;
     proposalEntity.snapshotBlock = parameters.snapshotBlock;
     proposalEntity.approvalReached = false;
 
     // Actions
-    let actions = vote.value.value3;
+    let actions = proposal.value.value3;
     for (let index = 0; index < actions.length; index++) {
       const action = actions[index];
 
