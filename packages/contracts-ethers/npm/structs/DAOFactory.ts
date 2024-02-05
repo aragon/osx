af50f11cbb14e0e0045794d8f03c4e43c8f14e3f
Michael Heuer <michael.heuer@rwth-aachen.de>
2024-02-05 12:11:38 +0100

    style: run prettier

--- packages/contracts-ethers/npm/structs/DAOFactory.ts
+++ packages/contracts-ethers/npm/structs/DAOFactory.ts
@@ -1,4 +1,4 @@
-import {DAOFactory} from '../../types/framework/dao/DAOFactory'
+import {DAOFactory} from '../../types/framework/dao/DAOFactory';
 
-export type DAOSettingsStruct = DAOFactory.DAOSettingsStruct
-export type PluginSettingsStruct = DAOFactory.PluginSettingsStruct
+export type DAOSettingsStruct = DAOFactory.DAOSettingsStruct;
+export type PluginSettingsStruct = DAOFactory.PluginSettingsStruct;
--- packages/contracts-ethers/npm/structs/PluginRepo.ts
+++ packages/contracts-ethers/npm/structs/PluginRepo.ts
@@ -2,4 +2,3 @@ import {PluginRepo} from '../../types/framework/plugin/repo/PluginRepo';
 
 export type TagStruct = PluginRepo.TagStruct;
 export type VersionStruct = PluginRepo.VersionStruct;
-
