2d6fed03e28a35546abb45fe8765f890c84611a3
Michael Heuer <michael.heuer@rwth-aachen.de>
2024-02-05 18:49:56 +0100

    fix: remove struct now located in osx-commons-contracts

--- packages/contracts-ethers/npm/structs/IPluginSetup.ts
+++
@@ -1,4 +0,0 @@
-import {IPluginSetup} from '../../types/framework/plugin/setup/IPluginSetup';
-
-export type PreparedSetupDataStruct = IPluginSetup.PreparedSetupDataStruct;
-export type SetupPayloadStruct = IPluginSetup.SetupPayloadStruct;
--- packages/contracts-ethers/npm/structs/index.ts
+++ packages/contracts-ethers/npm/structs/index.ts
@@ -1,5 +1,4 @@
 export * as DAOStructs from './DAO';
 export * as DAOFactoryStructs from './DAOFactory';
-export * as IPluginSetupStructs from './IPluginSetup';
 export * as PluginRepoStructs from './PluginRepo';
 export * as PluginSetupProcessorStructs from './PluginSetupProcessor';
