879a003241f32a09dcf23b80921b99cf60e2aec7
Claudia <claudiabarcelovaldes40@gmail.com>
2024-03-14 15:45:27 +0100

    feat: refactor configuration to use network configuration from common-configs

--- packages/contracts/networks.ts
+++ packages/contracts/networks.ts
@@ -1,50 +1,36 @@
-import {NetworkConfigs, networks} from '@aragon/osx-commons-configs';
-
-export type ContractsNetworkConfig = {
+type NetworkExtension = {
   deploy: string[];
 };
 
-const networkExtensions: NetworkConfigs<ContractsNetworkConfig> = {
+export const networkExtensions: {[index: string]: NetworkExtension} = {
   mainnet: {
-    ...networks.mainnet,
     deploy: ['./deploy/update/to_v1.3.0', './deploy/verification'],
   },
   goerli: {
-    ...networks.goerli,
     deploy: ['./deploy/update/to_v1.3.0', './deploy/verification'],
   },
   sepolia: {
-    ...networks.sepolia,
     deploy: ['./deploy/new', './deploy/verification'],
   },
   polygon: {
-    ...networks.polygon,
     deploy: ['./deploy/update/to_v1.3.0', './deploy/verification'],
   },
   mumbai: {
-    ...networks.mumbai,
     deploy: ['./deploy/update/to_v1.3.0', './deploy/verification'],
   },
   baseMainnet: {
-    ...networks.baseMainnet,
     deploy: ['./deploy/new', './deploy/verification'],
   },
   baseGoerli: {
-    ...networks.baseGoerli,
     deploy: ['./deploy/new', './deploy/verification'],
   },
   arbitrum: {
-    ...networks.arbitrum,
     deploy: ['./deploy/new', './deploy/verification'],
   },
   baseSepolia: {
-    ...networks.baseSepolia,
     deploy: ['./deploy/new', './deploy/verification'],
   },
   arbitrumSepolia: {
-    ...networks.arbitrumSepolia,
     deploy: ['./deploy/new', './deploy/verification'],
   },
 };
-
-export default networkExtensions;
