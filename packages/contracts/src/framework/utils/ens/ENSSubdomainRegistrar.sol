27cc52684f79443bfcd4069352f95d3b558f5296
Claudia <claudiabarcelovaldes40@gmail.com>
2024-04-29 14:36:27 +0200

    feat: update the subdomain registrar to follow new approach

--- packages/contracts/src/framework/utils/ens/ENSSubdomainRegistrar.sol
+++ packages/contracts/src/framework/utils/ens/ENSSubdomainRegistrar.sol
@@ -11,6 +11,10 @@ import {ProtocolVersion} from "@aragon/osx-commons-contracts/src/utils/versionin
 import {DaoAuthorizableUpgradeable} from "@aragon/osx-commons-contracts/src/permission/auth/DaoAuthorizableUpgradeable.sol";
 import {IDAO} from "@aragon/osx-commons-contracts/src/dao/IDAO.sol";
 
+import {DAORegistry} from "../../dao/DAORegistry.sol";
+import {PluginRepoRegistry} from "../../plugin/repo/PluginRepoRegistry.sol";
+import {PluginRepo} from "../../plugin/repo/PluginRepo.sol";
+
 /// @title ENSSubdomainRegistrar
 /// @author Aragon Association - 2022-2023
 /// @notice This contract registers ENS subdomains under a parent domain specified in the initialization process and maintains ownership of the subdomain since only the resolver address is set. This contract must either be the domain node owner or an approved operator of the node owner. The default resolver being used is the one specified in the parent domain.
@@ -33,15 +37,20 @@ contract ENSSubdomainRegistrar is UUPSUpgradeable, DaoAuthorizableUpgradeable, P
     /// @notice The address of the ENS resolver resolving the names to an address.
     address public resolver;
 
+    DAORegistry public daoRegistry;
+    PluginRepoRegistry public pluginRepoRegistry;
+
     /// @notice Thrown if the subnode is already registered.
     /// @param subnode The subnode namehash.
     /// @param nodeOwner The node owner address.
     error AlreadyRegistered(bytes32 subnode, address nodeOwner);
+    error NotRegistered(bytes32 subnode, address nodeOwner);
 
     /// @notice Thrown if node's resolver is invalid.
     /// @param node The node namehash.
     /// @param resolver The node resolver address.
     error InvalidResolver(bytes32 node, address resolver);
+    error Unauthorized();
 
     /// @dev Used to disallow initializing the implementation contract by an attacker for extra safety.
     /// @custom:oz-upgrades-unsafe-allow constructor
@@ -57,11 +66,19 @@ contract ENSSubdomainRegistrar is UUPSUpgradeable, DaoAuthorizableUpgradeable, P
     /// @param _managingDao The interface of the DAO managing the components permissions.
     /// @param _ens The interface of the ENS registry to be used.
     /// @param _node The ENS parent domain node under which the subdomains are to be registered.
-    function initialize(IDAO _managingDao, ENS _ens, bytes32 _node) external initializer {
+    function initialize(
+        IDAO _managingDao,
+        ENS _ens,
+        DAORegistry _daoRegistry,
+        PluginRepoRegistry _pluginRepoRegistry,
+        bytes32 _node
+    ) external initializer {
         __DaoAuthorizableUpgradeable_init(_managingDao);
 
         ens = _ens;
         node = _node;
+        daoRegistry = _daoRegistry;
+        pluginRepoRegistry = _pluginRepoRegistry;
 
         address nodeResolver = ens.resolver(_node);
 
@@ -78,6 +95,34 @@ contract ENSSubdomainRegistrar is UUPSUpgradeable, DaoAuthorizableUpgradeable, P
         address
     ) internal virtual override auth(UPGRADE_REGISTRAR_PERMISSION_ID) {}
 
+    modifier isAllowed(
+        bool isRegistered,
+        bool isPlugin,
+        address _targetAddress
+    ) {
+        if (!isRegistered) revert Unauthorized();
+
+        if (isPlugin) {
+            if (
+                !PluginRepo(_targetAddress).isGranted(
+                    _targetAddress,
+                    msg.sender,
+                    PluginRepo(_targetAddress).MAINTAINER_PERMISSION_ID(),
+                    bytes("")
+                )
+            ) revert Unauthorized();
+        }
+
+        _;
+    }
+
+    function registerSubnode(
+        bytes32 _label
+    ) external isAllowed(daoRegistry.entries(msg.sender), false, address(0)) {
+        // is a registered dao
+        _registerSubnode(_label, msg.sender);
+    }
+
     /// @notice Registers a new subdomain with this registrar as the owner and set the target address in the resolver.
     /// @dev It reverts with no message if this contract isn't the owner nor an approved operator for the given node.
     /// @param _label The labelhash of the subdomain name.
@@ -85,7 +130,12 @@ contract ENSSubdomainRegistrar is UUPSUpgradeable, DaoAuthorizableUpgradeable, P
     function registerSubnode(
         bytes32 _label,
         address _targetAddress
-    ) external auth(REGISTER_ENS_SUBDOMAIN_PERMISSION_ID) {
+    ) external isAllowed(daoRegistry.entries(msg.sender), true, _targetAddress) {
+        // is registered plugin and the caller has maintainer permission
+        _registerSubnode(_label, _targetAddress);
+    }
+
+    function _registerSubnode(bytes32 _label, address _targetAddress) internal {
         bytes32 subnode = keccak256(abi.encodePacked(node, _label));
         address currentOwner = ens.owner(subnode);
 
@@ -96,6 +146,39 @@ contract ENSSubdomainRegistrar is UUPSUpgradeable, DaoAuthorizableUpgradeable, P
         ens.setSubnodeOwner(node, _label, address(this));
         ens.setResolver(subnode, resolver);
         Resolver(resolver).setAddr(subnode, _targetAddress);
+
+        // TODO add the reverse registrar record as highlighted by Cristiano
+    }
+
+    function unregisterSubnode(
+        bytes32 _label
+    ) external isAllowed(daoRegistry.entries(msg.sender), false, address(0)) {
+        // is a registered dao
+        _unregisterSubnode(_label);
+    }
+
+    function unregisterSubnode(
+        bytes32 _label,
+        address _targetAddress
+    ) external isAllowed(daoRegistry.entries(msg.sender), true, _targetAddress) {
+        // is registered plugin and the caller has maintainer permission
+        _unregisterSubnode(_label);
+    }
+
+    function _unregisterSubnode(bytes32 _label) internal {
+        // ! mock implementation, need to review the correct way to unregister a subdomain
+        bytes32 subnode = keccak256(abi.encodePacked(node, _label));
+        address currentOwner = ens.owner(subnode);
+
+        if (currentOwner != address(this)) {
+            revert NotRegistered(subnode, currentOwner);
+        }
+
+        ens.setSubnodeOwner(node, _label, address(0));
+        ens.setResolver(subnode, address(0));
+        Resolver(resolver).setAddr(subnode, address(0));
+
+        // TODO unregister the reverse registrar
     }
 
     /// @notice Sets the default resolver contract address that the subdomains being registered will use.
