a19398682edf7096f1bea8602d07b9587e9e054a
Giorgi Lagidze <giorgilagidze@Giorgis-MacBook-Pro.local>
2024-04-30 13:23:07 +0400

    fix: yarn and imports

--- packages/contracts/deploy/helpers.ts
+++ packages/contracts/deploy/helpers.ts
@@ -1,3 +1,12 @@
+import {promises as fs} from 'fs';
+import {ethers} from 'hardhat';
+import {Contract} from 'ethers';
+import {SignerWithAddress} from '@nomiclabs/hardhat-ethers/signers';
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import IPFS from 'ipfs-http-client';
+
+import {findEvent, findEventTopicLog} from '../utils/event';
+import {Operation} from '../utils/types';
 import {VersionTag} from '../test/test-utils/psp/types';
 import {
   ENSRegistry__factory,
@@ -7,14 +16,6 @@ import {
 } from '../typechain';
 import {VersionCreatedEvent} from '../typechain/PluginRepo';
 import {PluginRepoRegisteredEvent} from '../typechain/PluginRepoRegistry';
-import {findEvent, findEventTopicLog} from '../utils/event';
-import {Operation} from '../utils/types';
-import {SignerWithAddress} from '@nomiclabs/hardhat-ethers/signers';
-import {Contract} from 'ethers';
-import {promises as fs} from 'fs';
-import {ethers} from 'hardhat';
-import {HardhatRuntimeEnvironment} from 'hardhat/types';
-import IPFS from 'ipfs-http-client';
 
 // TODO: Add support for L2 such as Arbitrum. (https://discuss.ens.domains/t/register-using-layer-2/688)
 // Make sure you own the ENS set in the {{NETWORK}}_ENS_DOMAIN variable in .env
