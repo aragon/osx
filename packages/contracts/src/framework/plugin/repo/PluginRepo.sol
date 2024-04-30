fce603a83e844597766f7b71fe0a8098dad7b429
Jose Manuel Mariñas Bascoy <josemarinasbascoy@gmail.com>
2024-04-30 10:20:47 +0200

    chore: add storage gap

--- packages/contracts/src/framework/plugin/repo/PluginRepo.sol
+++ packages/contracts/src/framework/plugin/repo/PluginRepo.sol
@@ -255,4 +255,7 @@ contract PluginRepo is
             _interfaceId == type(IProtocolVersion).interfaceId ||
             super.supportsInterface(_interfaceId);
     }
+
+    /// @notice This empty reserved space is put in place to allow future versions to add new variables without shifting down storage in the inheritance chain (see [OpenZeppelin's guide about storage gaps](https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps)).
+    uint256[46] private __gap;
 }
