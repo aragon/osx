c92d12938181c9d46e51112d20df277d96accded
Carles <75954325+banasa44@users.noreply.github.com>
2024-05-06 13:54:38 +0200

    ci: fixed workflow for generating introspection query for docs  (#578)
    * feat: .sh to generate a temp.schema to generate introspection.json
    
    * ci: rename file to avoid collisions
    
    * ci: move schema-introspection for query examples
    
    * fix: fix some comments in generate-intros-partial script
    
    * Update .gitignore
    
    Co-authored-by: Rekard0 <5880388+Rekard0@users.noreply.github.com>
    
    * Update .github/workflows/subgraph-documentation-update.yml
    
    Co-authored-by: Michael Heuer <20623991+Michael-A-Heuer@users.noreply.github.com>
    
    * chore: move scripts inside /cripts |- /docs and /tests folders
    
    ---------
    
    Co-authored-by: Rekard0 <5880388+Rekard0@users.noreply.github.com>
    Co-authored-by: Michael Heuer <20623991+Michael-A-Heuer@users.noreply.github.com>

--- packages/subgraph/scripts/tests/generate-introspection-json.sh
+++ packages/subgraph/scripts/tests/generate-introspection-json.sh
@@ -34,10 +34,10 @@ graph create $SUBGRAPH_NAME-$NETWORK_NAME --node http://localhost:8020
 # deploy subgraph
 LOCAL=true ./scripts/deploy-subgraph.sh
 # make introspection json
-FILENAME="docs/schema-introspection.json"
+FILENAME="tests/utils/schema-introspection.json"
 GRAPH_URL="http://localhost:8000/subgraphs/name/$SUBGRAPH_NAME-$NETWORK_NAME"
 echo "Fetching introspection query from $GRAPH_URL"
-node ./scripts/fetch-introspection-query.js $GRAPH_URL $FILENAME
+node ./scripts/tests/fetch-introspection-query.js $GRAPH_URL $FILENAME
 prettier $FILENAME --write
 # down docker
 docker compose -f docker/docker-compose.yml down
