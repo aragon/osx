0615dcbe9053ecaa96534bb3829c1ec8364cbd45
Claudia <claudiabarcelovaldes40@gmail.com>
2024-05-14 13:08:48 +0200

    fix: undo change in tests since it is testing a previous contract version

--- packages/contracts/test/upgrade/dao.ts
+++ packages/contracts/test/upgrade/dao.ts
@@ -311,7 +311,7 @@ describe('DAO Upgrade', function () {
       );
 
       await expect(daoV100.protocolVersion())
-        .to.be.revertedWithCustomError(daoV100Proxy, 'UnknownCallback')
+        .to.be.revertedWithCustomError(daoV100Proxy, 'UnkownCallback')
         .withArgs(protocolVersionSelector, '0x00000000');
 
       // for DAO v1.3.0 onward
