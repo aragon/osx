dce7cbe442f6b864dcb96951b2dfff53e32b384d
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2024-11-01 08:02:41 +0100

    fix broken imports (#620)

--- packages/contracts-ethers/npm/structs/DAO.ts
+++ packages/contracts-ethers/npm/structs/DAO.ts
@@ -1,7 +1,7 @@
-import {IDAO, PermissionLib} from '../../types/core/dao/DAO';
+// import {IDAO, PermissionLib} from '../../types/src/core/dao/DAO';
 
-export type ActionStruct = IDAO.ActionStruct;
-export type MultiTargetPermissionStruct =
-  PermissionLib.MultiTargetPermissionStruct;
-export type SingleTargetPermissionStruct =
-  PermissionLib.SingleTargetPermissionStruct;
+// export type ActionStruct = IDAO.ActionStruct;
+// export type MultiTargetPermissionStruct =
+//   PermissionLib.MultiTargetPermissionStruct;
+// export type SingleTargetPermissionStruct =
+//   PermissionLib.SingleTargetPermissionStruct;
