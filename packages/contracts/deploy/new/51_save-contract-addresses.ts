10626fb0a86ac0928aa3f4d8216c06e025502306
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2024-11-25 09:44:44 +0100

    fix saving contract key in json

--- packages/contracts/deploy/new/51_save-contract-addresses.ts
+++ packages/contracts/deploy/new/51_save-contract-addresses.ts
@@ -1,7 +1,7 @@
 import {DeployFunction} from 'hardhat-deploy/types';
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {promises as fs} from 'fs';
-import {DAOFactory} from '../../typechain';
+import {DAOFactory, IPluginSetup, PluginRepoFactory} from '../../typechain';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   console.log('\nPrinting deployed contracts.');
@@ -9,6 +9,8 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   const deployedContracts = await deployments.all();
   const deployedContractDetails: {[index: string]: any} = {};
+
+  let pluginRepoBaseDetail: any;
 
   for (const deploymentName in deployedContracts) {
     // Skip proxies because they are included twice
@@ -24,16 +26,18 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
       switch (deploymentName) {
         case 'DAO':
-          deployedContractDetails['managingDAO'] = contractDetails;
+          deployedContractDetails['ManagementDAOProxy'] = contractDetails;
           console.log(`Managing DAO: ${JSON.stringify(contractDetails)}`);
           break;
+
         case 'DAO_Implementation':
-          deployedContractDetails['managingDAOImplementation'] =
+          deployedContractDetails['ManagementDAOImplementation'] =
             contractDetails;
           console.log(
             `Managing DAO Implementation: ${JSON.stringify(contractDetails)}`
           );
           break;
+
         case 'DAOFactory':
           deployedContractDetails[deploymentName] = contractDetails;
           console.log(`${deploymentName}: ${JSON.stringify(contractDetails)}`);
@@ -45,31 +49,109 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
           )) as DAOFactory;
           const daoBaseAddress = await daoFactory.daoBase();
 
-          deployedContractDetails['DAOBase'] = {
+          pluginRepoBaseDetail = {
             address: daoBaseAddress,
             blockNumber: contractDetails.blockNumber,
             deploymentTx: contractDetails.deploymentTx,
           };
+
+          deployedContractDetails['DAOBase'] = pluginRepoBaseDetail;
+
           console.log(`DAOBase: ${daoBaseAddress}`);
           break;
-        default:
+
+        case 'PluginRepoFactory':
           deployedContractDetails[deploymentName] = contractDetails;
+          console.log(`${deploymentName}: ${JSON.stringify(contractDetails)}`);
+
+          // Call DAOFactory to get daoBase() address
+          const pluginRepoFactory = (await ethers.getContractAt(
+            'PluginRepoFactory',
+            address
+          )) as PluginRepoFactory;
+          const pluginRepoBaseAddress =
+            await pluginRepoFactory.pluginRepoBase();
+
+          deployedContractDetails['PluginRepoBase'] = {
+            address: pluginRepoBaseAddress,
+            blockNumber: contractDetails.blockNumber,
+            deploymentTx: contractDetails.deploymentTx,
+          };
+          console.log(`PluginRepoBase: ${pluginRepoBaseAddress}`);
+          break;
+
+        case 'AdminSetup':
+        case 'MultisigSetup':
+        case 'TokenVotingSetup':
+        case 'AddresslistVotingSetup': {
+          deployedContractDetails[deploymentName] = contractDetails;
+          console.log(`${deploymentName}: ${JSON.stringify(contractDetails)}`);
+
+          // Fetch the implementation address for the plugin setup
+          const contract = (await ethers.getContractAt(
+            deploymentName,
+            address
+          )) as IPluginSetup;
+          const implementationAddress = await contract.implementation();
+
+          deployedContractDetails[`${deploymentName}Implementation`] = {
+            address: implementationAddress,
+            blockNumber: contractDetails.blockNumber,
+            deploymentTx: contractDetails.deploymentTx,
+          };
+
+          console.log(
+            `${deploymentName} Implementation: ${JSON.stringify(
+              deployedContractDetails[`${deploymentName}Implementation`]
+            )}`
+          );
+          break;
+        }
+
+        default:
+          // Check and clean up the deployment name if it contains '_'
+          let cleanDeploymentName = deploymentName;
+          if (deploymentName.includes('_')) {
+            cleanDeploymentName = deploymentName.replace(/_/g, ''); // Remove all underscores
+          }
+
+          // Append 'Proxy' to specific names
+          const proxyNames = [
+            'PluginRepoRegistry',
+            'PluginENSSubdomainRegistrar',
+            'DAOENSSubdomainRegistrar',
+            'DAORegistry',
+          ];
+
+          if (proxyNames.includes(cleanDeploymentName)) {
+            // Add 'Proxy' suffix
+            cleanDeploymentName = `${cleanDeploymentName}Proxy`;
+          }
+
+          // Store the deployment details under the cleaned-up name
+          deployedContractDetails[cleanDeploymentName] = contractDetails;
+
           console.log(`${deploymentName}: ${JSON.stringify(contractDetails)}`);
       }
     }
   }
 
   for (const pluginRepo in aragonPluginRepos) {
-    deployedContractDetails[`${pluginRepo}-repo`] = {
+    const formatedRepoKey = formatReposKey(pluginRepo);
+    deployedContractDetails[`${formatedRepoKey}Proxy`] = {
       address: aragonPluginRepos[pluginRepo].address,
       blockNumber: aragonPluginRepos[pluginRepo].blockNumber,
       deploymentTx: aragonPluginRepos[pluginRepo].transactionHash,
     };
     console.log(
-      `${pluginRepo}-repo: ${JSON.stringify(
-        deployedContractDetails[`${pluginRepo}-repo`]
+      `${formatedRepoKey}Proxy: ${JSON.stringify(
+        deployedContractDetails[`${formatedRepoKey}Proxy`]
       )}`
     );
+
+    // Store PluginRepoBase as `formatedRepoKey` + `implementation`
+    deployedContractDetails[`${formatedRepoKey}Implementation`] =
+      pluginRepoBaseDetail;
   }
 
   await fs.writeFile(
@@ -77,6 +159,15 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     JSON.stringify(deployedContractDetails, null, 2)
   );
 };
+
+function formatReposKey(pluginRepo: string): string {
+  return (
+    pluginRepo
+      .split('-')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize each word
+      .join('') + 'Repo'
+  ); // Append "Repo"
+}
 
 export default func;
 func.tags = ['New', 'Conclude'];
