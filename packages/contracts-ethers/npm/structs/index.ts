21e3ff3fa3e6777c8b5bf2c97a919d852c03d726
Claudia <claudiabarcelovaldes40@gmail.com>
2025-01-22 14:55:19 +0100

    feat: export dao structs in osx ethers

--- packages/contracts-ethers/npm/structs/index.ts
+++ packages/contracts-ethers/npm/structs/index.ts
@@ -1,4 +1,4 @@
-// export * as DAOStructs from './DAO';
+export * as DAOStructs from './DAO';
 export * as DAOFactoryStructs from './DAOFactory';
 export * as PluginRepoStructs from './PluginRepo';
 export * as PluginSetupProcessorStructs from './PluginSetupProcessor';
