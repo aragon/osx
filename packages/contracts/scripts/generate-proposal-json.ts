258312838759a17536f779e90827096dd18fadb0
Claudia <claudiabarcelovaldes40@gmail.com>
2025-02-07 12:16:48 +0100

    feat: create the proposal creation call information to the script

--- packages/contracts/scripts/generate-proposal-json.ts
+++ packages/contracts/scripts/generate-proposal-json.ts
@@ -1,3 +1,6 @@
+// note using version 1.3.0 due to is the one currently installed on the managing dao change it once it's upgraded
+import {Multisig__factory as Multisig_v1_3_0__factory} from '../typechain/@aragon/osx-v1.3.0/plugins/governance/multisig/Multisig.sol';
+import {ethers} from 'ethers';
 import * as fs from 'fs';
 import * as path from 'path';
 
@@ -6,6 +9,12 @@ interface Action {
   value: number;
   data: string;
   description: string;
+}
+
+interface ProposalAction {
+  to: string;
+  value: number;
+  data: string;
 }
 
 const deployedContractsPath = path.join(
@@ -18,6 +27,8 @@ const mergedProposalActionsPath = path.join(
   __dirname,
   './merged-proposals.json'
 );
+
+const calldataPath = path.join(__dirname, './calldata.json');
 
 async function generateProposalJson() {
   try {
@@ -86,4 +97,103 @@ async function generateProposalJson() {
   }
 }
 
-generateProposalJson();
+export function generateHexCalldataInJson(functionArgs: any[]) {
+  try {
+    const abi = Multisig_v1_3_0__factory.abi;
+    const iface = new ethers.utils.Interface(abi);
+
+    const calldata = iface.encodeFunctionData('createProposal', functionArgs);
+
+    const jsonOutput = {
+      functionName: 'createProposal',
+      functionArgs: functionArgs,
+      calldata: calldata,
+    };
+
+    // write the call information in the json file
+    fs.writeFileSync(
+      calldataPath,
+      JSON.stringify(jsonOutput, null, 2),
+      'utf-8'
+    );
+    console.log(
+      'Successfully created calldata.json with the function call information!'
+    );
+  } catch (error) {
+    console.error('Error encoding function data:', error);
+    throw error;
+  }
+}
+
+function main() {
+  generateProposalJson();
+
+  // get the actions to send it to the createHexCalldata function
+  const jsonFile = JSON.parse(
+    fs.readFileSync(mergedProposalActionsPath, 'utf-8')
+  );
+
+  let args = [];
+  // push the metadata
+  if (!jsonFile.metadata) {
+    console.error('No metadata found in merged-proposals.json');
+    return 1;
+  }
+  args.push(ethers.utils.hexlify(ethers.utils.toUtf8Bytes(jsonFile.metadata)));
+
+  // push the actions
+  if (!jsonFile.managementDAOActions) {
+    console.error('No actions found in merged-proposals.json');
+    return 1;
+  }
+  // remove the description from the actions
+  let proposalActions: ProposalAction[] = [];
+  jsonFile.managementDAOActions.forEach((action: Action) => {
+    proposalActions.push({
+      to: action.to,
+      value: action.value,
+      data: action.data,
+    });
+  });
+  args.push(proposalActions);
+
+  // push allow failure map
+  if (
+    jsonFile.allowFailureMap === null ||
+    jsonFile.allowFailureMap === undefined
+  ) {
+    console.error('No allow failure map found in merged-proposals.json');
+    return 1;
+  }
+  args.push(jsonFile.allowFailureMap);
+
+  // push approve proposal
+  if (
+    jsonFile.approveProposal === null ||
+    jsonFile.approveProposal === undefined
+  ) {
+    console.error('No approve proposal found in merged-proposals.json');
+    return 1;
+  }
+  args.push(jsonFile.approveProposal);
+
+  // push try execution
+  if (jsonFile.tryExecution === null || jsonFile.tryExecution === undefined) {
+    console.error('No try execution found in merged-proposals.json');
+    return 1;
+  }
+  args.push(jsonFile.tryExecution);
+
+  // push the start and end dates
+  if (!jsonFile.startDate || !jsonFile.endDate) {
+    console.error('No start or end date found in merged-proposals.json');
+    return 1;
+  }
+  args.push(jsonFile.startDate);
+  args.push(jsonFile.endDate);
+
+  // generate the calldata in a json file
+  generateHexCalldataInJson(args);
+}
+
+main();
