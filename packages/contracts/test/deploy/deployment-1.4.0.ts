34ef747906e5fe5197352b4e305f7189d76ba2a2
Claudia <claudiabarcelovaldes40@gmail.com>
2025-02-11 15:10:33 +0100

    feat: add check for the permission and plugin and framework state after the proposal is executed

--- packages/contracts/test/deploy/deployment-1.4.0.ts
+++ packages/contracts/test/deploy/deployment-1.4.0.ts
@@ -1,8 +1,13 @@
 import {getLatestContractAddress} from '../../deploy/helpers';
+import {DAO__factory, PluginRepo__factory} from '../../typechain';
 import {Multisig__factory as Multisig_v1_3_0__factory} from '../../typechain/@aragon/osx-v1.3.0/plugins/governance/multisig/Multisig.sol';
 import {closeFork, initForkForOsxVersion} from '../test-utils/fixture';
-import {SignerWithAddress} from '@nomiclabs/hardhat-ethers/signers';
+import {
+  DAO_REGISTRY_PERMISSIONS,
+  PLUGIN_REGISTRY_PERMISSIONS,
+} from '@aragon/osx-commons-sdk';
 import {expect} from 'chai';
+import {defaultAbiCoder} from 'ethers/lib/utils';
 import * as fs from 'fs';
 import hre, {ethers} from 'hardhat';
 import * as path from 'path';
@@ -27,12 +32,36 @@ const daoMultisigMembers = [
   '0xdf62645a2c714febbf6060d1fb607e7eccef0659',
 ];
 
+const IMPLEMENTATION_ADDRESS_SLOT =
+  '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc';
+
+type OldAddresses = {
+  daoRegistry: string;
+  pluginRepoRegistry: string;
+  oldDaoFactory: string;
+  oldPluginRepoFactory: string;
+  oldDaoRegistryImplementation: string;
+  oldPluginRepoRegistryImplementation: string;
+};
+type Addresses = {
+  daoFactory: string;
+  pluginRepoFactory: string;
+  daoRegistryImplementation: string;
+  pluginRepoRegistryImplementation: string;
+  adminRepo: string;
+  tokenVotingRepo: string;
+  multisigRepo: string;
+  adminPluginSetup: string;
+  tokenVotingPluginSetup: string;
+  multisigPluginSetup: string;
+};
+
 async function forkNetwork(network: string) {
   hre.network.deploy = ['./deploy/update/to_v1.4.0'];
 
   await initForkForOsxVersion(network, {
     version: '1.3.0',
-    forkBlockNumber: 7680675, // todo adjust to latest block
+    forkBlockNumber: 7685985, // todo adjust to latest block
     activeContracts: [],
   });
 }
@@ -43,11 +72,59 @@ function getCalldataJson() {
 
   return calldataJson;
 }
+function getAddressFromDescription(description: string): string {
+  const address = description.split("at '")[1].split("' \n")[0];
+  return address;
+}
+
+function getAddress(name: string) {
+  return getLatestContractAddress(name, hre);
+}
+
+function getOldAddresses(): OldAddresses {
+  return {
+    daoRegistry: getAddress('DAORegistryProxy'),
+    pluginRepoRegistry: getAddress('PluginRepoRegistryProxy'),
+    oldDaoFactory: getAddress('DAOFactory'),
+    oldPluginRepoFactory: getAddress('PluginRepoFactory'),
+    oldDaoRegistryImplementation: getAddress('DAORegistryImplementation'),
+    oldPluginRepoRegistryImplementation: getAddress(
+      'PluginRepoRegistryImplementation'
+    ),
+  };
+}
+
+function getAddresses(): Addresses {
+  const addresses = JSON.parse(
+    fs.readFileSync(mergedProposalActionsPath, 'utf8')
+  );
+
+  return {
+    daoFactory: addresses.deployedContractAddresses.DAOFactory,
+    pluginRepoFactory: addresses.deployedContractAddresses.PluginRepoFactory,
+    daoRegistryImplementation:
+      addresses.deployedContractAddresses.DAORegistryImplementation,
+    pluginRepoRegistryImplementation:
+      addresses.deployedContractAddresses.PluginRepoRegistryImplementation,
+    adminRepo: addresses.managementDAOActions[5].to,
+    tokenVotingRepo: addresses.managementDAOActions[6].to,
+    multisigRepo: addresses.managementDAOActions[7].to,
+    adminPluginSetup: getAddressFromDescription(
+      addresses.managementDAOActions[5].description
+    ),
+    tokenVotingPluginSetup: getAddressFromDescription(
+      addresses.managementDAOActions[6].description
+    ),
+    multisigPluginSetup: getAddressFromDescription(
+      addresses.managementDAOActions[7].description
+    ),
+  };
+}
 
 async function impersonateAccount(addr: string) {
   await hre.network.provider.send('hardhat_setBalance', [
     addr,
-    ethers.utils.parseUnits('3', 'ether').toHexString(),
+    ethers.utils.parseUnits('3000', 'ether').toHexString(),
   ]);
 
   await hre.network.provider.request({
@@ -74,31 +151,164 @@ function getMultisigEvents(
 }
 
 // this function is deployment 1.4.0 specific adjust it for future deployments
-function checkStatusAfterProposal(proposal: any) {
-  // check permissions are correct
+async function checkStatusAfterProposal() {
+  // Actions
+  // 1- grant REGISTER_DAO_PERMISSION_ID to the new DAOFactory
+  // 2- grant REGISTER_PLUGIN_REPO_PERMISSION to the new PluginRepoFactory and revoke it on the old PluginRepoFactory
+  // 3- upgrade the DAORegistry implementation
+  // 4- upgrade the PluginRepoRegistry implementation
+  // 5- upgrade the managing DAO implementation
+  // 6- deploy new admin version
+  // 7- deploy new token voting version
+  // 8- deploy new multisig version
+
+  const member0 = await impersonateAccount(daoMultisigMembers[0]);
+  const dao = DAO__factory.connect(daoAddress, member0);
+
+  const addresses = getAddresses();
+  const oldAddresses = getOldAddresses();
 
   // new dao factory has REGISTER_DAO_PERMISSION_ID on the DAORegistry
+  expect(
+    await dao.hasPermission(
+      oldAddresses.daoRegistry, // where
+      addresses.daoFactory, // who
+      DAO_REGISTRY_PERMISSIONS.REGISTER_DAO_PERMISSION_ID, // permission id
+      '0x' // data
+    ),
+    'new dao factory permission'
+  ).to.be.true;
+
   // old dao factory has REGISTER_DAO_PERMISSION_ID on the DAORegistry
+  expect(
+    await dao.hasPermission(
+      oldAddresses.daoRegistry, // where
+      oldAddresses.oldDaoFactory, // who
+      DAO_REGISTRY_PERMISSIONS.REGISTER_DAO_PERMISSION_ID, // permission id
+      '0x' // data
+    ),
+    'old dao factory permission'
+  ).to.be.true;
 
   // new repo factory has REGISTER_PLUGIN_REPO_PERMISSION on the PluginRepoRegistry
+  expect(
+    await dao.hasPermission(
+      oldAddresses.pluginRepoRegistry, // where
+      addresses.pluginRepoFactory, // who
+      PLUGIN_REGISTRY_PERMISSIONS.REGISTER_PLUGIN_REPO_PERMISSION_ID, // permission id
+      '0x' // data
+    ),
+    'new repo factory permission'
+  ).to.be.true;
+
   // old repo factory has not REGISTER_PLUGIN_REPO_PERMISSION on the PluginRepoRegistry
+  expect(
+    await dao.hasPermission(
+      oldAddresses.pluginRepoRegistry, // where
+      oldAddresses.oldPluginRepoFactory, // who
+      PLUGIN_REGISTRY_PERMISSIONS.REGISTER_PLUGIN_REPO_PERMISSION_ID, // permission id
+      '0x' // data
+    ),
+    'old repo factory permission'
+  ).to.be.false;
 
   // check the dao registry implementation has changed
+  const newDaoRegistryImplementation = defaultAbiCoder
+    .decode(
+      ['address'],
+      await ethers.provider.getStorageAt(
+        oldAddresses.daoRegistry,
+        IMPLEMENTATION_ADDRESS_SLOT
+      )
+    )[0]
+    .toLowerCase();
+
+  expect(
+    newDaoRegistryImplementation,
+    'dao registry implementation'
+  ).not.to.equal(oldAddresses.oldDaoRegistryImplementation);
+  expect(
+    newDaoRegistryImplementation.toLowerCase(),
+    'new dao registry implementation'
+  ).to.equal(addresses.daoRegistryImplementation.toLowerCase());
 
   // check the plugin repo registry implementation has changed
+  const newPluginRepoRegistryImplementation = defaultAbiCoder
+    .decode(
+      ['address'],
+      await ethers.provider.getStorageAt(
+        oldAddresses.pluginRepoRegistry,
+        IMPLEMENTATION_ADDRESS_SLOT
+      )
+    )[0]
+    .toLowerCase();
 
-  expect(proposal.executed).to.be.true;
+  expect(
+    newPluginRepoRegistryImplementation,
+    'plugin repo registry implementation'
+  ).not.to.equal(oldAddresses.oldPluginRepoRegistryImplementation);
+  expect(
+    newPluginRepoRegistryImplementation.toLowerCase(),
+    'new plugin repo registry implementation'
+  ).to.equal(addresses.pluginRepoRegistryImplementation.toLowerCase());
+
+  // management dao implementation (version) has changed
+  expect(await dao.protocolVersion(), 'managing dao version').to.deep.equal([
+    1, 4, 0,
+  ]);
+
+  // check new admin version is deployed with correct setup
+  const adminRepo = PluginRepo__factory.connect(addresses.adminRepo, member0);
+  const adminLatestVersion = await adminRepo['getLatestVersion(uint8)'](1);
+
+  expect(adminLatestVersion.tag.release, 'admin release').to.equal(1);
+  expect(adminLatestVersion.tag.build, 'admin build').to.equal(2);
+  expect(adminLatestVersion.pluginSetup, 'admin setup').to.deep.equal(
+    addresses.adminPluginSetup
+  );
+
+  // check new token voting version is deployed with correct setup
+  const tokenVotingRepo = PluginRepo__factory.connect(
+    addresses.tokenVotingRepo,
+    member0
+  );
+  const tokenVotingLatestVersion = await tokenVotingRepo[
+    'getLatestVersion(uint8)'
+  ](1);
+
+  expect(tokenVotingLatestVersion.tag.release, 'tokenVoting release').to.equal(
+    1
+  );
+  expect(tokenVotingLatestVersion.tag.build, 'tokenVoting build').to.equal(3);
+  expect(tokenVotingLatestVersion.pluginSetup, 'tokenVoting setup').to.equal(
+    addresses.tokenVotingPluginSetup
+  );
+
+  // check new multisig version is deployed with correct setup
+  const multisigRepo = PluginRepo__factory.connect(
+    addresses.multisigRepo,
+    member0
+  );
+  const multisigLatestVersion = await multisigRepo['getLatestVersion(uint8)'](
+    1
+  );
+
+  expect(multisigLatestVersion.tag.release, 'multisig release').to.equal(1);
+  expect(multisigLatestVersion.tag.build, 'multisig build').to.equal(3);
+  expect(multisigLatestVersion.pluginSetup, 'multisig setup').to.equal(
+    addresses.multisigPluginSetup
+  );
 }
 
+// todo think when do this this test because it should only run when testing deployment
 describe.only('1.4.0 Deployment', function () {
   let calldataJson: any;
 
   beforeEach(async () => {
     await forkNetwork(NETWORK);
+    console.log('forked network: ', NETWORK);
 
     calldataJson = getCalldataJson();
-
-    [deployer] = await ethers.getSigners();
   });
 
   // Close fork so that other tests(not related to this file) are
@@ -118,6 +328,7 @@ describe.only('1.4.0 Deployment', function () {
     let tx = await member0.sendTransaction({
       to: multisig.address,
       data: calldataJson.calldata,
+      gasLimit: 3000000,
     });
 
     let receipt = await tx.wait();
@@ -130,8 +341,6 @@ describe.only('1.4.0 Deployment', function () {
     );
 
     // check proposal created event
-
-    // get the event
     let proposalCreatedEvent = getMultisigEvents(
       receipt,
       'ProposalCreated',
@@ -196,16 +405,13 @@ describe.only('1.4.0 Deployment', function () {
     tx = await multisig.execute(proposalId);
     receipt = await tx.wait();
 
-    // todo think on a way to parse all events
+    // todo think on a way to parse all events or if it is worthy
 
     // check the proposal is executed
     proposal = await multisig.getProposal(proposalId);
     expect(proposal.executed).to.be.true;
 
-    // todo add check for
-    // 1- permissions are correct
-    // 1.1- new factories and old factories have permission
-    // 2- all plugins new versions are created
+    await checkStatusAfterProposal();
   });
 
   it('execute all the proposal actions one by one', async () => {
@@ -220,9 +426,6 @@ describe.only('1.4.0 Deployment', function () {
       });
     }
 
-    // todo add check for
-    // 1- permissions are correct
-    // 1.1- new factories and old factories have permission
-    // 2- all plugins new versions are created
+    await checkStatusAfterProposal();
   });
 });
