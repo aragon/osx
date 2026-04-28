f82f2e4cfc51cac733198ba721565ce85bf3579f
Jør∂¡ <itsbrickpop@gmail.com>
2026-04-28 15:58:56 +0000

    Refactor round 1: removing contracts-ethers

--- packages/contracts-ethers/npm/structs/DAOFactory.ts
+++
@@ -1,4 +0,0 @@
-import {DAOFactory} from '../../types/src/framework/dao/DAOFactory';
-
-export type DAOSettingsStruct = DAOFactory.DAOSettingsStruct;
-export type PluginSettingsStruct = DAOFactory.PluginSettingsStruct;
--- packages/contracts-ethers/npm/structs/index.ts
+++
@@ -1,4 +0,0 @@
-export * as DAOStructs from './DAO';
-export * as DAOFactoryStructs from './DAOFactory';
-export * as PluginRepoStructs from './PluginRepo';
-export * as PluginSetupProcessorStructs from './PluginSetupProcessor';
